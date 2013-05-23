97cebff13561eb97f6fb241222c35828ed8af791
Sascha Heymann <sascha.heymann@gmail.com>
2013-05-23 11:10:01 +0000

    -Bug solved for negative numbers starts with zero e.g. -0.123 -Bug solved for numbers starts with zero after the dot e.g. 0.00123 -Add float precision adjustable e.g. %.3f = 0.123

--- utils/src/eprintf.c
+++ utils/src/eprintf.c
@@ -31,28 +31,55 @@
 static const char digit[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 
                              'A', 'B', 'C', 'D', 'E', 'F'};
 
-static int itoa(putc_t putcf, int num, int base)
+int get_int_len (int value)
+{
+  int l=1;
+  while(value>9)
+  {
+    l++;
+    value/=10;
+  }
+  return l;
+}
+
+int power(int a, int b)
+{
+  int i;
+  int x=a;
+
+  for (i=1; i<b; i++)
+  {
+    x*=a;
+  }
+  return x;
+}
+
+static int itoa(putc_t putcf, int num, int base, int precision)
 {
   long long int i=1;
   int len=0;
   unsigned int n = num;
+  int mumLenght = get_int_len(num);
+  int fillWithZero = 0;
 
   if (num==0)
   {
     putcf('0');
     return 1;
   }
-
-  if(base==10 && num<0) 
+  if (mumLenght < precision)
   {
-    n = -num;
-
-    putcf('-');
-    len++;
+    fillWithZero = precision -mumLenght;
+    while (fillWithZero>0)
+    {
+      putcf('0');
+      len++;
+      fillWithZero--;
+    }
   }
 
   while (n/i)
-    i*=base;
+  i*=base;
 
   while(i/=base)
   {
@@ -68,28 +95,42 @@ int evprintf(putc_t putcf, char * fmt, va_list ap)
   int len=0;
   float num;
   char* str;
+  int precision;
 
   while (*fmt)
   {
+    precision = 6;
     if (*fmt == '%')
     {
-      while(!isalpha((unsigned)*++fmt));//TODO: Implement basic print length handling!
+      while(!isalpha((unsigned)*++fmt))//TODO: Implement basic print length handling!
+      {
+        if (*fmt == '.')
+        {
+          if (isdigit((unsigned)*++fmt))
+            precision = *fmt - '0';
+        }
+      }
       switch (*fmt++)
       {
         case 'i':
         case 'd':
-          len += itoa(putcf, va_arg( ap, int ), 10);
+          len += itoa(putcf, va_arg( ap, int ), 10 , 0);
           break;
         case 'x':
         case 'X':
-          len += itoa(putcf, va_arg( ap, int ), 16);
+          len += itoa(putcf, va_arg( ap, int ), 16 , 0);
           break;
         case 'f':
           num = va_arg( ap, double );
-          len += itoa(putcf, (int)num, 10);
+          if(num<0)
+          {
+            putcf('-');
+            num=-num;
+            len++;
+          }
+          len += itoa(putcf, (int)num, 10, 0);
           putcf('.'); len++;
-          if(num<0) num=-num;
-          len += itoa(putcf, (num-(int)num)*10e5, 10);
+          len += itoa(putcf, (num-(int)num)*power(10,precision), 10, precision);
           break;
         case 's':
           str = va_arg( ap, char* );
@@ -123,4 +164,3 @@ int eprintf(putc_t putcf, char * fmt, ...)
 
   return len;
 }
-
