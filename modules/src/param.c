fa21ffa034ad5103b1b19a6d187b81746bde38d9
Arnaud Taffanel <arnaud@bitcraze.se>
2015-06-17 15:01:44 +0200

    Param: Implemented set param by name command

--- modules/src/param.c
+++ modules/src/param.c
@@ -40,6 +40,7 @@
 #define TOC_CH 0
 #define READ_CH 1
 #define WRITE_CH 2
+#define MISC_CH 3
 
 #define CMD_RESET 0
 #define CMD_GET_NEXT 1
@@ -47,6 +48,8 @@
 
 #define CMD_GET_ITEM 0
 #define CMD_GET_INFO 1
+
+#define MISC_SETBYNAME 0
 
 //Private functions
 static void paramTask(void * prm);
@@ -61,6 +64,7 @@ extern struct param_s _param_stop;
 static void paramWriteProcess(int id, void*);
 static void paramReadProcess(int id);
 static int variableGetIndex(int id);
+static char paramWriteByNameProcess(char* group, char* name, int type, void *valptr);
 
 //Pointer to the parameters list and length of it
 static struct param_s * params;
@@ -80,20 +84,20 @@ void paramInit(void)
   params = &_param_start;
   paramsLen = &_param_stop - &_param_start;
   paramsCrc = crcSlow(params, paramsLen);
-  
+
   for (i=0; i<paramsLen; i++)
   {
-    if(!(params[i].type & PARAM_GROUP)) 
+    if(!(params[i].type & PARAM_GROUP))
       paramsCount++;
   }
-  
-  
+
+
   //Start the param task
 	xTaskCreate(paramTask, (const signed char * const)PARAM_TASK_NAME,
 	            PARAM_TASK_STACKSIZE, NULL, PARAM_TASK_PRI, NULL);
-  
+
   //TODO: Handle stored parameters!
-  
+
   isInit = true;
 }
 
@@ -107,16 +111,45 @@ CRTPPacket p;
 void paramTask(void * prm)
 {
 	crtpInitTaskQueue(CRTP_PORT_PARAM);
-	
+
 	while(1) {
 		crtpReceivePacketBlock(CRTP_PORT_PARAM, &p);
-		
+
 		if (p.channel==TOC_CH)
 		  paramTOCProcess(p.data[0]);
 	  else if (p.channel==READ_CH)
 		  paramReadProcess(p.data[0]);
 		else if (p.channel==WRITE_CH)
 		  paramWriteProcess(p.data[0], &p.data[1]);
+    else if (p.channel==MISC_CH) {
+      if (p.data[0] == MISC_SETBYNAME) {
+        int i, nzero = 0;
+        char *group;
+        char *name;
+        uint8_t type;
+        void * valPtr;
+        int error;
+
+        // If the packet contains at least 2 zeros in the first 28 bytes
+        // The packet decoding algorithm will not crash
+        for (i=0; i<31; i++) {
+          if (p.data[i] == '\0') nzero++;
+        }
+
+        if (nzero < 2) return;
+
+        group = (char*)&p.data[1];
+        name = (char*)&p.data[1+strlen(group)+1];
+        type = p.data[1+strlen(group)+1+strlen(name)+1];
+        valPtr = &p.data[1+strlen(group)+1+strlen(name)+2];
+
+        error = paramWriteByNameProcess(group, name, type, valPtr);
+
+        p.data[1+strlen(group)+1+strlen(name)+1] = error;
+        p.size = 1+strlen(group)+1+strlen(name)+1+1;
+        crtpSendPacket(&p);
+      }
+    }
 	}
 }
 
@@ -125,7 +158,7 @@ void paramTOCProcess(int command)
   int ptr = 0;
   char * group = "";
   int n=0;
-  
+
   switch (command)
   {
   case CMD_GET_INFO: //Get info packet about the param implementation
@@ -155,7 +188,7 @@ void paramTOCProcess(int command)
         n++;
       }
     }
-    
+
     if (ptr<paramsLen)
     {
       p.header=CRTP_HEADER(CRTP_PORT_PARAM, TOC_CH);
@@ -165,7 +198,7 @@ void paramTOCProcess(int command)
       memcpy(p.data+3, group, strlen(group)+1);
       memcpy(p.data+3+strlen(group)+1, params[ptr].name, strlen(params[ptr].name)+1);
       p.size=3+2+strlen(group)+strlen(params[ptr].name);
-      crtpSendPacket(&p);      
+      crtpSendPacket(&p);
     } else {
       p.header=CRTP_HEADER(CRTP_PORT_PARAM, TOC_CH);
       p.data[0]=CMD_GET_ITEM;
@@ -181,13 +214,13 @@ static void paramWriteProcess(int ident, void* valptr)
   int id;
 
   id = variableGetIndex(ident);
-  
+
   if (id<0) {
     p.data[0] = -1;
     p.data[1] = ident;
     p.data[2] = ENOENT;
     p.size = 3;
-    
+
     crtpSendPacket(&p);
     return;
   }
@@ -210,8 +243,59 @@ static void paramWriteProcess(int ident, void* valptr)
       *(uint64_t*)params[id].address = *(uint64_t*)valptr;
       break;
   }
-  
+
   crtpSendPacket(&p);
+}
+
+static char paramWriteByNameProcess(char* group, char* name, int type, void *valptr) {
+  int ptr;
+  char *pgroup = "";
+
+  for (ptr=0; ptr<paramsLen; ptr++) //Ptr points a group
+  {
+    if (params[ptr].type & PARAM_GROUP)
+    {
+      if (params[ptr].type & PARAM_START)
+        pgroup = params[ptr].name;
+      else
+        pgroup = "";
+    }
+    else                          //Ptr points a variable
+    {
+      if (!strcmp(params[ptr].name, name) && !strcmp(pgroup, group))
+        break;
+    }
+  }
+
+  if (ptr >= paramsLen) {
+    return ENOENT;
+  }
+
+  if (type != params[ptr].type) {
+    return EINVAL;
+  }
+
+  if (params[ptr].type & PARAM_RONLY) {
+    return EACCES;
+  }
+
+  switch (params[ptr].type & PARAM_BYTES_MASK)
+  {
+ 	case PARAM_1BYTE:
+ 		*(uint8_t*)params[ptr].address = *(uint8_t*)valptr;
+ 		break;
+    case PARAM_2BYTES:
+  	  *(uint16_t*)params[ptr].address = *(uint16_t*)valptr;
+      break;
+ 	case PARAM_4BYTES:
+      *(uint32_t*)params[ptr].address = *(uint32_t*)valptr;
+      break;
+ 	case PARAM_8BYTES:
+      *(uint64_t*)params[ptr].address = *(uint64_t*)valptr;
+      break;
+  }
+
+  return 0;
 }
 
 static void paramReadProcess(int ident)
@@ -219,13 +303,13 @@ static void paramReadProcess(int ident)
   int id;
 
   id = variableGetIndex(ident);
-  
+
   if (id<0) {
     p.data[0] = -1;
     p.data[1] = ident;
     p.data[2] = ENOENT;
     p.size = 3;
-    
+
     crtpSendPacket(&p);
     return;
   }
@@ -250,7 +334,7 @@ static void paramReadProcess(int ident)
    		p.size = 1+sizeof(uint64_t);
    		break;
   }
-  
+
   crtpSendPacket(&p);
 }
 
@@ -258,21 +342,19 @@ static int variableGetIndex(int id)
 {
   int i;
   int n=0;
-  
+
   for (i=0; i<paramsLen; i++)
   {
-    if(!(params[i].type & PARAM_GROUP)) 
+    if(!(params[i].type & PARAM_GROUP))
     {
       if(n==id)
         break;
       n++;
     }
   }
-  
+
   if (i>=paramsLen)
     return -1;
-  
+
   return i;
 }
-
-
