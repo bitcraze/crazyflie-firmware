b8a857e855450de98404a4bc4d6bdc05c56d3de1
Tobias Antonsson <tobias@bitcraze.se>
2015-12-17 15:34:34 +0100

    Rebase which fixes CF1 DEBUG=1 build. Fixed missing INPUT_PULLDOWN after rebase.

--- deck/interface/deck_constants.h
+++ deck/interface/deck_constants.h
@@ -35,9 +35,10 @@
 #define LOW 0x0
 #define HIGH 0x1
 
-#define INPUT 0x0
-#define OUTPUT 0x1
-#define INPUT_PULLUP 0x2
+#define INPUT           0x0
+#define OUTPUT          0x1
+#define INPUT_PULLUP    0x2
+#define INPUT_PULLDOWN  0x3
 
 /*
  * The Deck port GPIO pins, as named by the deck port / expansion port / expansion breakout documentation on bitcraze.io.
