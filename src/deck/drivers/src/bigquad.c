9485b728ae09a4bc3f3dffef8b5634ce2016f46e
Arnaud Taffanel <arnaud@bitcraze.se>
2016-05-12 11:17:59 +0200

    Make PM in bigquad driver optional
    We cannot use the PM features and the LPS deck at the same time
    so PM is now optional

--- src/deck/drivers/src/bigquad.c
+++ src/deck/drivers/src/bigquad.c
@@ -52,8 +52,10 @@ static void bigquadInit(DeckInfo *info)
   DEBUG_PRINT("Switching to brushless.\n");
   motorsInit(motorMapBigQuadDeck);
   extRxInit();
+#ifdef BQ_DECK_ENABLE_PM
   pmEnableExtBatteryVoltMeasuring(BIGQUAD_BAT_VOLT_PIN, BIGQUAD_BAT_VOLT_MULT);
   pmEnableExtBatteryCurrMeasuring(BIGQUAD_BAT_CURR_PIN, BIGQUAD_BAT_AMP_PER_VOLT);
+#endif
 
   isInit = true;
 }
