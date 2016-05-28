a0628ec2c171c0cac18bdf5306101c697ebae590
Arnaud Taffanel <arnaud@bitcraze.se>
2016-05-28 15:35:50 +0200

    Added new experimental attitude PID settings to CF2
    Added experimental PID settings that results in a faster and tighter
    control of the CF2 attitude. Rate PID found by @theseankelly, see post on 
    the forum: https://forum.bitcraze.io/viewtopic.php?f=6&t=1978
    
    To enable the new settings att the folowing to config.mk
    CFLAGS+=-DEXPERIMENTAL_ATTITUDE_PID

--- src/modules/interface/pid.h
+++ src/modules/interface/pid.h
@@ -29,6 +29,43 @@
 
 #include <stdbool.h>
 
+#if defined(PLATFORM_CF2) && defined(EXPERIMENTAL_ATTITUDE_PID)
+
+#define PID_ROLL_RATE_KP  250.0
+#define PID_ROLL_RATE_KI  500.0
+#define PID_ROLL_RATE_KD  2.5
+#define PID_ROLL_RATE_INTEGRATION_LIMIT    33.3
+
+#define PID_PITCH_RATE_KP  250.0
+#define PID_PITCH_RATE_KI  500.0
+#define PID_PITCH_RATE_KD  2.5
+#define PID_PITCH_RATE_INTEGRATION_LIMIT   33.3
+
+#define PID_YAW_RATE_KP  70.0
+#define PID_YAW_RATE_KI  16.7
+#define PID_YAW_RATE_KD  0.0
+#define PID_YAW_RATE_INTEGRATION_LIMIT     166.7
+
+#define PID_ROLL_KP  10
+#define PID_ROLL_KI  4.0
+#define PID_ROLL_KD  0.0
+#define PID_ROLL_INTEGRATION_LIMIT    20.0
+
+#define PID_PITCH_KP  10
+#define PID_PITCH_KI  4.0
+#define PID_PITCH_KD  0.0
+#define PID_PITCH_INTEGRATION_LIMIT   20.0
+
+#define PID_YAW_KP  10.0
+#define PID_YAW_KI  1.0
+#define PID_YAW_KD  0.35
+#define PID_YAW_INTEGRATION_LIMIT     360.0
+
+
+#define DEFAULT_PID_INTEGRATION_LIMIT  5000.0
+
+#else
+
 #define PID_ROLL_RATE_KP  70.0
 #define PID_ROLL_RATE_KI  0.0
 #define PID_ROLL_RATE_KD  0.0
@@ -61,6 +98,8 @@
 
 
 #define DEFAULT_PID_INTEGRATION_LIMIT  5000.0
+
+#endif
 
 typedef struct
 {
