b18053e40f2bd4a4184a1e51fd9ce5cb07e25948
Stephan Brown <stephan.brown.07@gmail.com>
2016-10-10 11:51:11 -0700

    sensors_cf2: Remove vl53l0x items from file.

--- src/hal/src/sensors_cf2.c
+++ src/hal/src/sensors_cf2.c
@@ -38,7 +38,6 @@
 #include "lps25h.h"
 #include "mpu6500.h"
 #include "ak8963.h"
-#include "vl53l0x.h"
 
 #include "FreeRTOS.h"
 #include "semphr.h"
@@ -110,7 +109,6 @@ static xQueueHandle gyroDataQueue;
 static xQueueHandle magnetometerDataQueue;
 static xQueueHandle barometerDataQueue;
 static xSemaphoreHandle sensorsDataReady;
-static uint16_t range;
 
 static bool isInit = false;
 static sensorData_t sensors;
@@ -219,11 +217,6 @@ static void sensorsTask(void *param)
           processBarometerMeasurements(&(buffer[isMagnetometerPresent ?
                   SENSORS_MPU6500_BUFF_LEN + SENSORS_MAG_BUFF_LEN : SENSORS_MPU6500_BUFF_LEN]));
       }
-      uint16_t range_raw = vl53l0xReadRangeContinuousMillimeters();
-      if (range_raw < 8000) {
-        range = range_raw;
-      }
-
       vTaskSuspendAll(); // ensure all queues are populated at the same time
       xQueueOverwrite(accelerometerDataQueue, &sensors.acc);
       xQueueOverwrite(gyroDataQueue, &sensors.gyro);
@@ -313,19 +306,6 @@ static void sensorsDeviceInit(void)
 
   // Wait for sensors to startup
   while (xTaskGetTickCount() < 1000);
-
-  i2cdevInit(I2C1_DEV);
-  vl53l0xInit(I2C1_DEV);
-  if (vl53l0xTestConnection() == true)
-  {
-    DEBUG_PRINT("VL53L0X I2C connection [OK].\n");
-    vl53l0xInitSensor(true);
-    vl53l0xStartContinuous(0);
-  }
-  else
-  {
-    DEBUG_PRINT("VL53L0X I2C connection [FAIL].\n");
-  }
 
   i2cdevInit(I2C3_DEV);
   mpu6500Init(I2C3_DEV);
@@ -881,10 +861,6 @@ static void sensorsAccApplyLpf(Axis3f* in)
     in->axis[i] = lpf2pApply(&accLpf[i], in->axis[i]);
   }
 }
-
-LOG_GROUP_START(range)
-LOG_ADD(LOG_UINT16, range, &range)
-LOG_GROUP_STOP(range)
 
 PARAM_GROUP_START(imu_sensors)
 PARAM_ADD(PARAM_UINT8 | PARAM_RONLY, HMC5883L, &isMagnetometerPresent)
