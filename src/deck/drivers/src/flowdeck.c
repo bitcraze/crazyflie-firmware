69d5c424026b229ae8aa86e022c183879873cd52
Arnaud Taffanel <arnaud@bitcraze.se>
2017-06-14 14:04:41 +0200

    #234: Add license header to flow deck driver

--- src/deck/drivers/src/flowdeck.c
+++ src/deck/drivers/src/flowdeck.c
@@ -1,4 +1,28 @@
-// PA_Motion deck driver
+/*
+ *    ||          ____  _ __
+ * +------+      / __ )(_) /_______________ _____  ___
+ * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * LPS node firmware.
+ *
+ * Copyright 2017, Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* flowdeck.c: Flow deck driver */
 #include "deck.h"
 #include "debug.h"
 #include "log.h"
