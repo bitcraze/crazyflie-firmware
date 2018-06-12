2de29c243f713c940b6d8870a9818d87ae68ed2b
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-06-12 16:41:47 +0200

    Improved existing tests Added tests for the clockCorrectionEngine

--- test/deck/drivers/src/TestClockCorrection.c
+++ test/deck/drivers/src/TestClockCorrection.c
@@ -17,9 +17,9 @@ void tearDown(void) {
 
 void testGetClockCorrection() {
   // Fixture
-  double expectedResult = 12345.6789;
+  double clockCorrection = 12345.6789;
   clockCorrectionStorage_t clockCorrectionStorage = {
-    .clockCorrection = expectedResult,
+    .clockCorrection = clockCorrection,
     .clockCorrectionBucket = 0
   };
 
@@ -27,45 +27,46 @@ void testGetClockCorrection() {
   double result = getClockCorrection(&clockCorrectionStorage);
 
   // Assert
+  double expectedResult = clockCorrection;
   TEST_ASSERT_EQUAL_DOUBLE(expectedResult, result);
 }
 
-void testTruncateTimeStampFromDW1000WithBigInputAnd40BitsMask() {
+void testTruncateTimeStampWithBigInputAnd40BitsMask() {
   // Fixture
   uint64_t input = 0xABCDEFABCDEFABCD;
-  uint64_t expectedResult = 0x000000ABCDEFABCD;
   uint64_t mask = 0xFFFFFFFFFF; // 40 bits
 
   // Test
   uint64_t result = truncateTimeStamp(input, mask);
 
   // Assert
+  uint64_t expectedResult = 0x000000ABCDEFABCD;
   TEST_ASSERT_EQUAL_UINT64(expectedResult, result);
 }
 
-void testTruncateTimeStampFromDW1000WithBigInputAnd30BitsMask() {
+void testTruncateTimeStampWithBigInputAnd32BitsMask() {
   // Fixture
   uint64_t input = 0xABCDEFABCDEFABCD;
-  uint64_t expectedResult = 0x000000000DEFABCD;
-  uint64_t mask = 0x3FFFFFFF; // 30 bits
+  uint64_t mask = 0xFFFFFFFF; // 32 bits
 
   // Test
   uint64_t result = truncateTimeStamp(input, mask);
 
   // Assert
+  uint64_t expectedResult = 0x00000000CDEFABCD;
   TEST_ASSERT_EQUAL_UINT64(expectedResult, result);
 }
 
-void testTruncateTimeStampFromDW1000WithSmallInputAnd40BitsMask() {
+void testTruncateTimeStampWithSmallInputAnd40BitsMask() {
   // Fixture
   uint64_t input = 0x0000000000012345;
-  uint64_t expectedResult = 0x0000000000012345;
   uint64_t mask = 0xFFFFFFFFFF; // 40 bits
 
   // Test
   uint64_t result = truncateTimeStamp(input, mask);
 
   // Assert
+  uint64_t expectedResult = 0x0000000000012345;
   TEST_ASSERT_EQUAL_UINT64(expectedResult, result);
 }
 
@@ -111,53 +112,78 @@ void testEmptyClockCorrectionBucket() {
   }
 }
 
-void testCalculateClockCorrectionWithValidInputData() {
+void testCalculateClockCorrectionWithValidInputDataWithoutWrapAround() {
   // Fixture
-  double expectedClockCorrection = 1.005;
-  uint64_t old_t_in_cl_x = 1000;
-  uint64_t new_t_in_cl_x = 2000;
-  uint64_t old_t_in_cl_reference = old_t_in_cl_x * expectedClockCorrection;
-  uint64_t new_t_in_cl_reference = new_t_in_cl_x * expectedClockCorrection;
+  double clockCorrection = 1.0057;
   uint64_t mask = 0xFFFFFFFFFF; // 40 bits
+  uint64_t difference_in_cl_x = 10000;
+
+  uint64_t old_t_in_cl_x = 1000;
+  uint64_t new_t_in_cl_x = old_t_in_cl_x + difference_in_cl_x; // Does not wrap around
+  uint64_t old_t_in_cl_reference = 56789;
+  uint64_t new_t_in_cl_reference = old_t_in_cl_reference + clockCorrection * difference_in_cl_x; // Does not wrap around
 
   // Test
   double result = calculateClockCorrection(new_t_in_cl_reference, old_t_in_cl_reference, new_t_in_cl_x, old_t_in_cl_x, mask);
 
   // Assert
+  double expectedClockCorrection = clockCorrection;
+  TEST_ASSERT_EQUAL_DOUBLE(expectedClockCorrection, result);
+}
+
+void testCalculateClockCorrectionWithValidInputDataWithWrapAround() {
+  // Fixture
+  double clockCorrection = 1.0057;
+  uint64_t mask = 0xFFFFFFFFFF; // 40 bits
+  uint64_t difference_in_cl_x = 10000;
+
+  uint64_t old_t_in_cl_x = mask - difference_in_cl_x / 2;
+  uint64_t new_t_in_cl_x = (old_t_in_cl_x + difference_in_cl_x) & mask; // Wraps around
+  uint64_t old_t_in_cl_reference = 56789;
+  uint64_t new_t_in_cl_reference = old_t_in_cl_reference + clockCorrection * difference_in_cl_x; // Does not wrap around
+
+  // Test
+  double result = calculateClockCorrection(new_t_in_cl_reference, old_t_in_cl_reference, new_t_in_cl_x, old_t_in_cl_x, mask);
+
+  // Assert
+  double expectedClockCorrection = clockCorrection;
   TEST_ASSERT_EQUAL_DOUBLE(expectedClockCorrection, result);
 }
 
 void testCalculateClockCorrectionWithInvalidInputData() {
   // Fixture
-  double expectedClockCorrection = -1;
-  double clockCorrection = 1.005;
+  uint64_t mask = 0xFFFFFFFFFF; // 40 bits
+
   uint64_t old_t_in_cl_x = 1000;
   uint64_t new_t_in_cl_x = 1000;
-  uint64_t old_t_in_cl_reference = old_t_in_cl_x * clockCorrection;
-  uint64_t new_t_in_cl_reference = new_t_in_cl_x * clockCorrection;
-  uint64_t mask = 0xFFFFFFFFFF; // 40 bits
+  uint64_t old_t_in_cl_reference = 56789;
+  uint64_t new_t_in_cl_reference = 56789;
 
   // Test
   double result = calculateClockCorrection(new_t_in_cl_reference, old_t_in_cl_reference, new_t_in_cl_x, old_t_in_cl_x, mask);
 
   // Assert
+  double expectedClockCorrection = -1;
   TEST_ASSERT_EQUAL_DOUBLE(expectedClockCorrection, result);
 }
 
 void testUpdateClockCorrectionWithSampleInTheOuterLimitOfTheSpecs() {
   // Fixture
-  double expectedClockCorrection = 5;
-  unsigned int expectedClockCorrectionBucket = 3;
-  clockCorrectionStorage_t clockCorrectionStorage = {
-    .clockCorrection = expectedClockCorrection,
-    .clockCorrectionBucket = expectedClockCorrectionBucket
-  };
+  double clockCorrection = 5;
+  unsigned int clockCorrectionBucket = 3;
   double clockCorrectionCandidate = CLOCK_CORRECTION_SPEC_MAX; // First value out of the specs
+
+  clockCorrectionStorage_t clockCorrectionStorage = {
+    .clockCorrection = clockCorrection,
+    .clockCorrectionBucket = clockCorrectionBucket
+  };
 
   // Test
   double result = updateClockCorrection(&clockCorrectionStorage, clockCorrectionCandidate);
 
   // Assert
+  double expectedClockCorrection = clockCorrection;
+  unsigned int expectedClockCorrectionBucket = clockCorrectionBucket;
   TEST_ASSERT_FALSE(result);
   TEST_ASSERT_EQUAL_DOUBLE(expectedClockCorrection, clockCorrectionStorage.clockCorrection);
   TEST_ASSERT_EQUAL_UINT(expectedClockCorrectionBucket, clockCorrectionStorage.clockCorrectionBucket);
@@ -165,18 +191,21 @@ void testUpdateClockCorrectionWithSampleInTheOuterLimitOfTheSpecs() {
 
 void testUpdateClockCorrectionWithSampleInTheInnerLimitOfTheSpecsWithEmptyBucket() {
   // Fixture
-  double expectedClockCorrection = nextafter(CLOCK_CORRECTION_SPEC_MAX, 1); // First value in the specs
-  unsigned int expectedClockCorrectionBucket = 1;
+  double clockCorrection = 1;
+  unsigned int clockCorrectionBucket = 0;
+  double clockCorrectionCandidate = nextafter(CLOCK_CORRECTION_SPEC_MAX, 1); // First value in the specs
+
   clockCorrectionStorage_t clockCorrectionStorage = {
-    .clockCorrection = 1,
-    .clockCorrectionBucket = 0
+    .clockCorrection = clockCorrection,
+    .clockCorrectionBucket = clockCorrectionBucket
   };
-  double clockCorrectionCandidate = expectedClockCorrection;
 
   // Test
   double result = updateClockCorrection(&clockCorrectionStorage, clockCorrectionCandidate);
 
   // Assert
+  double expectedClockCorrection = clockCorrectionCandidate;
+  unsigned int expectedClockCorrectionBucket = clockCorrectionBucket + 1;
   TEST_ASSERT_TRUE(result);
   TEST_ASSERT_EQUAL_DOUBLE(expectedClockCorrection, clockCorrectionStorage.clockCorrection);
   TEST_ASSERT_EQUAL_UINT(expectedClockCorrectionBucket, clockCorrectionStorage.clockCorrectionBucket);
@@ -184,18 +213,21 @@ void testUpdateClockCorrectionWithSampleInTheInnerLimitOfTheSpecsWithEmptyBucket
 
 void testUpdateClockCorrectionWithSampleInTheOuterLimitOfTheAcceptableNoiseWithEmptyBucket() {
   // Fixture
-  unsigned int expectedClockCorrectionBucket = 1;
+  double clockCorrection = 1;
+  unsigned int clockCorrectionBucket = 0;
+  double clockCorrectionCandidate = clockCorrection + CLOCK_CORRECTION_ACCEPTED_NOISE; // First value out of acceptable noise
+
   clockCorrectionStorage_t clockCorrectionStorage = {
-    .clockCorrection = 1,
-    .clockCorrectionBucket = 0
+    .clockCorrection = clockCorrection,
+    .clockCorrectionBucket = clockCorrectionBucket
   };
-  double expectedClockCorrection = clockCorrectionStorage.clockCorrection + CLOCK_CORRECTION_ACCEPTED_NOISE; // First value out of acceptable noise
-  double clockCorrectionCandidate = expectedClockCorrection;
 
   // Test
   double result = updateClockCorrection(&clockCorrectionStorage, clockCorrectionCandidate);
 
   // Assert
+  double expectedClockCorrection = clockCorrectionCandidate;
+  unsigned int expectedClockCorrectionBucket = clockCorrectionBucket + 1;
   TEST_ASSERT_TRUE(result);
   TEST_ASSERT_EQUAL_DOUBLE(expectedClockCorrection, clockCorrectionStorage.clockCorrection);
   TEST_ASSERT_EQUAL_UINT(expectedClockCorrectionBucket, clockCorrectionStorage.clockCorrectionBucket);
@@ -203,18 +235,21 @@ void testUpdateClockCorrectionWithSampleInTheOuterLimitOfTheAcceptableNoiseWithE
 
 void testUpdateClockCorrectionWithSampleInTheOuterLimitOfTheAcceptableNoiseWithNonEmptyBucket() {
   // Fixture
-  double expectedClockCorrection = 1;
-  unsigned int expectedClockCorrectionBucket = 1;
+  double clockCorrection = 1;
+  unsigned int clockCorrectionBucket = 2;
+  double clockCorrectionCandidate = clockCorrection + CLOCK_CORRECTION_ACCEPTED_NOISE; // First value out of acceptable noise
+
   clockCorrectionStorage_t clockCorrectionStorage = {
-    .clockCorrection = expectedClockCorrection,
-    .clockCorrectionBucket = 2
+    .clockCorrection = clockCorrection,
+    .clockCorrectionBucket = clockCorrectionBucket
   };
-  double clockCorrectionCandidate = clockCorrectionStorage.clockCorrection + CLOCK_CORRECTION_ACCEPTED_NOISE; // First value out of acceptable noise
 
   // Test
   double result = updateClockCorrection(&clockCorrectionStorage, clockCorrectionCandidate);
 
   // Assert
+  double expectedClockCorrection = clockCorrection;
+  unsigned int expectedClockCorrectionBucket = clockCorrectionBucket - 1;
   TEST_ASSERT_FALSE(result);
   TEST_ASSERT_EQUAL_DOUBLE(expectedClockCorrection, clockCorrectionStorage.clockCorrection);
   TEST_ASSERT_EQUAL_UINT(expectedClockCorrectionBucket, clockCorrectionStorage.clockCorrectionBucket);
@@ -222,18 +257,22 @@ void testUpdateClockCorrectionWithSampleInTheOuterLimitOfTheAcceptableNoiseWithN
 
 void testUpdateClockCorrectionWithSampleInTheInnerLimitOfTheAcceptableNoise() {
   // Fixture
-  unsigned int expectedClockCorrectionBucket = 3;
+
+  double clockCorrection = 1.0 + 10e-6; // A value inside the clock specs
+  unsigned int clockCorrectionBucket = 2;
+  double clockCorrectionCandidate = nextafter(clockCorrection + CLOCK_CORRECTION_ACCEPTED_NOISE, 1); // First value in the acceptable noise
+
   clockCorrectionStorage_t clockCorrectionStorage = {
-    .clockCorrection = 1.0 + 10e-6, // A value inside the clock specs
-    .clockCorrectionBucket = 2
+    .clockCorrection = clockCorrection,
+    .clockCorrectionBucket = clockCorrectionBucket
   };
-  double clockCorrectionCandidate = nextafter(clockCorrectionStorage.clockCorrection + CLOCK_CORRECTION_ACCEPTED_NOISE, 1); // First value in the acceptable noise
-  double expectedClockCorrection = clockCorrectionStorage.clockCorrection * CLOCK_CORRECTION_FILTER + clockCorrectionCandidate * (1.0 - CLOCK_CORRECTION_FILTER);
 
   // Test
   double result = updateClockCorrection(&clockCorrectionStorage, clockCorrectionCandidate);
 
   // Assert
+  double expectedClockCorrection = clockCorrection * CLOCK_CORRECTION_FILTER + clockCorrectionCandidate * (1.0 - CLOCK_CORRECTION_FILTER);
+  unsigned int expectedClockCorrectionBucket = clockCorrectionBucket + 1;
   TEST_ASSERT_TRUE(result);
   TEST_ASSERT_EQUAL_DOUBLE(expectedClockCorrection, clockCorrectionStorage.clockCorrection);
   TEST_ASSERT_EQUAL_UINT(expectedClockCorrectionBucket, clockCorrectionStorage.clockCorrectionBucket);
