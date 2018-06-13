2bd22e4d2e8d1e371bcff30453d4dfac5123c6ed
Andrés Cecilia Luque <a.cecilia.luque@gmail.com>
2018-06-13 21:00:06 +0200

    Better documentation

--- src/utils/interface/clockCorrection/clockCorrectionFunctions.h
+++ src/utils/interface/clockCorrection/clockCorrectionFunctions.h
@@ -8,11 +8,14 @@ Exposes all the functions used for clock correction, for testing purposes.
 #include <stdbool.h>
 #include "clockCorrectionStorage.h"
 
+/* Public */
 double getClockCorrection(clockCorrectionStorage_t* storage);
-uint64_t truncateTimeStamp(uint64_t timeStamp, uint64_t mask);
-void fillClockCorrectionBucket(clockCorrectionStorage_t* storage);
-bool emptyClockCorrectionBucket(clockCorrectionStorage_t* storage);
 double calculateClockCorrection(const uint64_t new_t_in_cl_reference, const uint64_t old_t_in_cl_reference, const uint64_t new_t_in_cl_x, const uint64_t old_t_in_cl_x, const uint64_t mask);
 bool updateClockCorrection(clockCorrectionStorage_t* storage, const double clockCorrectionCandidate);
+
+/* Private */
+uint64_t truncateTimeStamp(const uint64_t timeStamp, const uint64_t mask);
+void fillClockCorrectionBucket(clockCorrectionStorage_t* storage);
+bool emptyClockCorrectionBucket(clockCorrectionStorage_t* storage);
 
 #endif /* clockCorrectionFunctions_h */
