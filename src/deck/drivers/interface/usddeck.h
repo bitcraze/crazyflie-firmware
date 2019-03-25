0f85a4b7bcf1988c08404e45e88609de232fe462
Wolfgang Hoenig <whoenig@usc.edu>
2019-03-25 14:17:56 -0700

    uSD card: Add synchronous logging mode.
    The synchronous logging mode can log variables synchronously in the
    stabilizer loop. This helps to analyze flight crashes etc. where
    the order and consistency between variables is crucial. The older
    (asynchronous mode) is retained, although the config file format
    changed slightly.
    
    Tested on a CF 2.0 (both synchronous and asynchronous modes using
    the config.txt that is part of this repository.)

--- src/deck/drivers/interface/usddeck.h
+++ src/deck/drivers/interface/usddeck.h
@@ -4,23 +4,23 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct usdLogDataPtr_s {
-  uint32_t* tick;
-  uint8_t* data;
-} usdLogQueuePtr_t;
+enum usddeckLoggingMode_e
+{
+  usddeckLoggingMode_Disabled = 0,
+  usddeckLoggingMode_SynchronousStabilizer,
+  usddeckLoggingMode_Asyncronous,
+};
 
-typedef struct usdLogConfig_s {
-  char filename[13];
-  uint8_t items;
-  uint16_t frequency;
-  uint8_t bufferSize;
-  uint16_t numSlots;
-  uint16_t numBytes;
-  int* varIds; // dynamically allocated
-} usdLogConfig_t;
+// returns true if logging is enabled
+bool usddeckLoggingEnabled(void);
 
-#define USD_WRITE(FILE, MESSAGE, BYTES, BYTES_WRITTEN, CRC_VALUE, CRC_FINALXOR, CRC_TABLE) \
-  f_write(FILE, MESSAGE, BYTES, BYTES_WRITTEN); \
-  CRC_VALUE = crcByByte(MESSAGE, BYTES, CRC_VALUE, CRC_FINALXOR, CRC_TABLE);
+// returns the current logging mode
+enum usddeckLoggingMode_e usddeckLoggingMode(void);
+
+// returns the desired logging frequency
+int usddeckFrequency(void);
+
+// For synchronous logging: add a new log entry
+void usddeckTriggerLogging(void);
 
 #endif //__USDDECK_H__
