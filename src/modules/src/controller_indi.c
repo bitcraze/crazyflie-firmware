59d21276347e12810fd77fad2457b45188269960
Andre Paraense <andre.paraense@plymouth.ac.uk>
2019-07-30 10:09:51 +0100

    correcting license

--- src/modules/src/controller_indi.c
+++ src/modules/src/controller_indi.c
@@ -1,29 +1,19 @@
 /*
-The MIT License (MIT)
-
-Copyright (c) 2019 Andre Luis Ogando Paraense and Ewoud Smeur
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
-
-/*
+ *
+ * Copyright (c) 2019 Andre Luis Ogando Paraense and Ewoud Smeur
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
  * This control algorithm is the Incremental Nonlinear Dynamic Inversion (INDI)
  * controller.
  *
@@ -50,5 +40,16 @@ void controllerINDI(control_t *control, setpoint_t *setpoint,
                                          const state_t *state,
                                          const uint32_t tick)
 {
+
+	/*
+	 * 1 - Update the gyro filter with the new measurements.
+	 * 2 - Calculate the derivative with finite difference.
+	 * 3 - same filter on the actuators (or control_t values), using the commands from the previous timestep.
+	 * 3.1 - Calculate the desired angular acceleration by:
+	 * 3.2 - Rate_reference = P * attitude_error, where attitude error can be calculated with your favorite algorithm. You may even use a function that is already there, such as attitudeControllerCorrectAttitudePID(), though this will be inaccurate for large attitude errors, but it will be ok for now.
+	 * 4. Angular_acceleration_reference = D * (rate_reference – rate_measurement)
+	 * 5. Update the For each axis: delta_command = 1/control_effectiveness * (angular_acceleration_reference – angular_acceleration)
+	 * 6. Add delta_commands to commands and bound to allowable values
+	 */
 
 }
