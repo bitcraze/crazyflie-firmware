76b19e5cc9d6e5112f517bb6a8e7475821d8517a
Nickson <hi@nickson.me>
2019-09-08 20:36:45 +0200

    Cleanup lighthouse.c - removed some counting

--- src/deck/drivers/src/lighthouse.c
+++ src/deck/drivers/src/lighthouse.c
@@ -65,13 +65,13 @@
   #define DISABLE_LIGHTHOUSE_DRIVER 1
 #endif
 
-//with respect to MATLAB
+//Origins and Rotation Matrixes converted to World, because Rotation Matrix from Kalman Estimator is wrt World
 baseStationGeometry_t lighthouseBaseStationsGeometry[2]  = {
 {.origin = { 1.736229,  2.611738, 2.682860, }, .mat = {{-0.855681, -0.345626, 0.385167, }, {0.516858, -0.607955, 0.602701, }, {0.025856, 0.714796, 0.698855, }, }},
 {.origin = {-1.372339, -2.375781, 2.739366, }, .mat = {{0.840995, 0.307949, -0.444853, }, {-0.534727, 0.598345, -0.596699, }, {0.082423, 0.739696, 0.667874, }, }},
 };
 
-//with respect to MATLAB
+//Sensor Positions wrt World
 vec3d lighthouseSensorsGeometry[PULSE_PROCESSOR_N_SENSORS] = {
 		{-0.0150,  0.0075, 0},
 		{-0.0150, -0.0075, 0},
@@ -133,16 +133,12 @@ static bool comSynchronized = false;
 
 static uint16_t serialFrameCount = 0;
 static uint16_t frameCount = 0;
-static uint16_t cycleCount = 0;
 static uint16_t positionCount = 0;
-static uint16_t rayCount = 0;
 
 static float serialFrameRate = 0.0;
 static float frameRate = 0.0;
 static float cycleRate = 0.0;
 static float positionRate = 0.0;
-static float rayRate = 0.0;
-
 //static uint16_t pulseWidth[PULSE_PROCESSOR_N_SENSORS];
 
 static uint32_t latestStatsTimeMs = 0;
@@ -172,89 +168,40 @@ static bool getFrame(frame_t *frame)
 static void resetStats() {
   serialFrameCount = 0;
   frameCount = 0;
-  cycleCount = 0;
   positionCount = 0;
-  rayCount = 0;
 }
 
 static void calculateStats(uint32_t nowMs) {
   double time = (nowMs - latestStatsTimeMs) / 1000.0;
   serialFrameRate = serialFrameCount / time;
   frameRate = frameCount / time;
-  cycleRate = cycleCount / time;
   positionRate = positionCount / time;
-  rayRate = rayCount / time;
 
   resetStats();
 }
 
-//static vec3d positions[PULSE_PROCESSOR_N_SENSORS];
-//static bool positionsIsValid[PULSE_PROCESSOR_N_SENSORS];
-
 static positionMeasurement_t ext_pos;
 static float deltaLog;
-
-/*static void estimatePosition(pulseProcessorResult_t angles[]) {
-  memset(&ext_pos, 0, sizeof(ext_pos));
-  uint8_t sensorsUsed = 0;
-
-  // Average over all sensors with valid data
-  for (uint8_t sensor = 0; sensor < PULSE_PROCESSOR_N_SENSORS; sensor++) {
-//	  positionsIsValid[sensor] = false;
-		if (angles[sensor].validCount == 4) { //single sensor has gotten x & y axis from both basestations (2*2=4)
-			//it is possible to get position from single basestation, using multiple sensors
-			vec3d position;
-			float delta;
-			lighthouseGeometryGetPosition(lighthouseBaseStationsGeometry, (void*)angles[sensor].correctedAngles, position, &delta); //requires two base stations to be in view
-
-			deltaLog = delta;
-
-//			memcpy(positions[sensor], position, vec3d_size);
-//			positionsIsValid[sensor] = true;
-
-			ext_pos.x -= position[2];
-			ext_pos.y -= position[0];
-			ext_pos.z += position[1];
-			sensorsUsed++;
-
-			positionCount++;
-		}
-  }
-
-  ext_pos.x /= sensorsUsed;
-  ext_pos.y /= sensorsUsed;
-  ext_pos.z /= sensorsUsed;
-
-  // Make sure we feed sane data into the estimator
-  if (!isfinite(ext_pos.pos[0]) || !isfinite(ext_pos.pos[1]) || !isfinite(ext_pos.pos[2])) {
-    return;
-  }
-  ext_pos.stdDev = 0.01;
-  estimatorEnqueuePosition(&ext_pos);
-}*/
-
 
 typedef struct ray_s {
   uint8_t sensor;
   uint8_t baseStation;
-//  vec3d origin;
-//  vec3d direction;
 } __attribute__((packed)) ray_t;
 
 
-void estimatePosition2(pulseProcessor_t *state, pulseProcessorResult_t angles[])
+void estimatePosition(pulseProcessor_t *state, pulseProcessorResult_t angles[])
 {
 
 	#define MAX_RAYS PULSE_PROCESSOR_N_SENSORS*2  //given 2 base stations and 4 sensors, only possible to obtain in total of 8 unique rays
-  ray_t rays[MAX_RAYS] = {0};
+  ray_t rays[MAX_RAYS] = {0}; //stores all possible rays between all & and basestations
 
 	uint8_t rays_count = 0;
-	uint8_t sensorsInvolved = 0;
-	bool isBaseStationInvolved[2] = {0};
+	uint8_t uniqueSensorsInvolved = 0; //number of different sensors to be involved in the position calculation
+	bool isBaseStationInvolved[2] = {0}; //different basestations to be involved
 	{
 		uint32_t startT = T2M(xTaskGetTickCount());
 		for (uint8_t sensor = 0; sensor < PULSE_PROCESSOR_N_SENSORS; sensor++) {
-			bool isSensorInvolved = false;
+			bool isSensorInvolved = false; //has the sensor in this loop been involved already
 			for (uint8_t baseStation = 0; baseStation < 2; baseStation++) {
 
 				uint8_t validAxisCount  = 0;
@@ -267,7 +214,7 @@ void estimatePosition2(pulseProcessor_t *state, pulseProcessorResult_t angles[])
 	//				if(timestamp != 0 && timestamp_i != 0) {
 					if(timestamp_i != 0) {
 
-
+  //				// possible need to compare timestamps from FPGA point of view, but overflows in 11 sec
 	//				sweepDirection_j = X = 0
 	//				sweepDirection_k = Y = 1
 	//				uint32_t currentSync;
@@ -282,24 +229,15 @@ void estimatePosition2(pulseProcessor_t *state, pulseProcessorResult_t angles[])
 	//				}
 	//				uint32_t currentSync = state->currentSync;
 
-	//				int delta = TS_DIFF(urrentSync, timestamp);
-						int delta_i = startT - timestamp_i;
+	//				int delta = TS_DIFF(currentSync, timestamp); //delta wrt FPGA
+						int delta_i = startT - timestamp_i; //delta wrt CF
 
 	//					if(delta < 400000){ //1 sweep/frame/axis only
 	//					if(delta < 400000*2){ //2 sweeps/frames/axes, 1 cycle
-	//					if(delta < 400000*2*2){ //2 cycles //enough time only for 1 basestation, but all axis
-	//					if(delta < 48000000){ //1 second, 48000000/(400000*2) = 60 cycles
-	//					if(delta_i < 17){ //previous readings just slightly older than 1 cycle (more accurately 1/60*1000)
+
+//					if(delta_i < 27){//previous readings just slightly older than 1.5 cycles (more accurately 1/60*1000 * 1.5) (min steps: 0, 8/9, 16/17, 25/26)
 	//					if(delta_i < 34){//previous readings just slightly older than 2 cycles (more accurately 1/60*1000 * 2)
-	//						validAxisCount++;//should be 60hz per sensor
-	//					}
-	//					if(delta_i < 67){//previous readings just slightly older than 4 cycles (more accurately 1/60*1000 * 4)
-	//					if(delta_i < 1000){//previous readings cannot be older than 1 sec
-	//						validAxisCount++;//should be 60hz per sensor
-	//					}
-	//					if(delta_i < 27){//previous readings just slightly older than 1.5 cycles (more accurately 1/60*1000 * 1.5) (min steps: 0, 8/9, 16/17, 25/26)
-	//						validAxisCount++;//should be 60hz per sensor
-	//					}
+
 						if(delta_i < 31){//previous readings cannot never be older than 2 cycles (1/60*1000 * 2), but capture all possible 1.5 cycles (more accurately 1/60*1000 * 1.5) (min steps: 0, 8/9, 16/17, 25/26)
 							validAxisCount++;//should be 60hz per sensor
 						}
@@ -308,24 +246,19 @@ void estimatePosition2(pulseProcessor_t *state, pulseProcessorResult_t angles[])
 
 				if(validAxisCount >= 2){
 
-//					vec3d direction, origin; //TODO: very weirdly, passing rays[rays_count].direction, rays[rays_count].origin by reference causes error
-		//			pulseProcessorApplyCalibration(state, angles); //apply calibration only when needed
-					pulseProcessorApplyCalibration2(state, angles, baseStation, sensor); //apply calibration only when needed
-//					calc_ray_vec(&lighthouseBaseStationsGeometry[baseStation], angles[sensor].correctedAngles[baseStation][0], angles[sensor].correctedAngles[baseStation][1], direction, origin);
-
+					pulseProcessorApplyCalibration2(state, angles, baseStation, sensor); //makes sure the angles for the sensor and basestation are calibrated
+					//TODO: should cache the calibrations
 
 					rays[rays_count].sensor = sensor;
 					rays[rays_count].baseStation = baseStation;
-	//				memcpy(rays[rays_count].origin, origin, sizeof(vec3d));
-//					memcpy(rays[rays_count].direction, direction, sizeof(vec3d));
 
 					rays_count++;
 
-					isBaseStationInvolved[baseStation] = true;
+					isBaseStationInvolved[baseStation] = true; //particular basestation was involved
 
 					if(!isSensorInvolved){
-						isSensorInvolved = true;
-						sensorsInvolved++;
+						isSensorInvolved = true; //the sensor in this loop been involved already
+						uniqueSensorsInvolved++; //increment the number of different sensors involved
 					}
 
 				}
@@ -335,16 +268,12 @@ void estimatePosition2(pulseProcessor_t *state, pulseProcessorResult_t angles[])
 		}
 	}
 
-	rayCount += rays_count;
-	//not necessarily unique rays per round, but out of all the rays, one of them will surely be new,
-	//so the combination of rays is unique as a whole, but some may have been detected in previous invocation
+	//for every invocation, not necessarily rays are unique, but out of all the rays, one of them will surely be new (angle from an axis was updated)
+	//so the combination of rays is unique as a whole (some may have been used in previous invocation)
 
-//  if(rays_count >= 8){ // also produces 960Hz of Rays only when rays_count
-//  	rayCount += rays_count;
-//  }
+	bool noSingleBaseStation = isBaseStationInvolved[0] && isBaseStationInvolved[1] && uniqueSensorsInvolved >= 2; //condition to ignore pairs of rays from only 1 BS (error prone)
 
-//  if(rays_count >= 8){ //test if 8 rays at 1 go were possible
-	bool noSingleBaseStation = isBaseStationInvolved[0] && isBaseStationInvolved[1] && sensorsInvolved >= 2;
+	//  if(rays_count >= 8){ //test if all 8 rays at 1 go was possible
   if(rays_count >= 2){ //require at least two rays
 
     uint8_t ray_pairs_count = 0;
@@ -365,17 +294,16 @@ void estimatePosition2(pulseProcessor_t *state, pulseProcessorResult_t angles[])
 					static vec3d D = {0}; //0 by default, likely rays fall on same sensor
 
 					if(rays[i].sensor != rays[j].sensor){ //if rays do not fall on same sensor, find the vector between sensors
-						float R[3][3];
+						float R[3][3]; //Estimated rotation matrix of the Sensors
 						estimatorKalmanGetEstimatedRotationMatrix(R);
 						arm_matrix_instance_f32 R_mat = {3, 3, R};
 
-//						vec3d S = {};
+//						vec3d S = {}; //the vector between the 2 sensors
 //						arm_sub_f32(lighthouseSensorsGeometry[rays[j].sensor], lighthouseSensorsGeometry[rays[i].sensor], S, vec3d_size);
 //						arm_matrix_instance_f32 S_mat = {3, 1, S};
+
 						arm_matrix_instance_f32 S_mat = {3, 1, S[rays[j].sensor][rays[i].sensor]}; //use pre-computed values for speed
-
 						arm_matrix_instance_f32 D_mat = {3, 1, D};
-
 						arm_mat_mult_f32(&R_mat, &S_mat, &D_mat);
 					}
 
@@ -418,13 +346,13 @@ void estimatePosition2(pulseProcessor_t *state, pulseProcessorResult_t angles[])
 
 
 						if(ray_pairs_count >= 12){
-							break; //just stop if more than 12
+							break; //just stop if more than 12 (mainly if using values from both basestations)
 						}
 					}
 				}
 		  }
 			if(ray_pairs_count >= 12){
-				break; //just stop if more than 12
+				break; //just stop if more than 12 (mainly if using values from both basestations)
 			}
 	  }
 
@@ -445,12 +373,12 @@ void estimatePosition2(pulseProcessor_t *state, pulseProcessorResult_t angles[])
 		  if(noSingleBaseStation){
 		  	ext_pos.stdDev = 0.01;
 		  }else{
-			  ext_pos.stdDev = 0.05; //since single basestation is possible, don't trust it that much
+			  ext_pos.stdDev = 0.05; //since position may be from single basestation, don't trust it that much
 		  }
 		  estimatorEnqueuePosition(&ext_pos);
 
 		  //			uint32_t endT = T2M(xTaskGetTickCount());
-		  //			uint32_t deltaT = endT - startT;
+		  //			uint32_t deltaT = endT - startT; //estimate time taken for all the pairs for rays and averaging
 
 		}
   }
@@ -520,32 +448,16 @@ static void lighthouseTask(void *param)
     while(synchronized) {
       if (frame.sync != 0) {
         synchronized = getFrame(&frame);
-//        memset(pulseWidth, 0, sizeof(pulseWidth[0])*PULSE_PROCESSOR_N_SENSORS);
         continue;
       }
 
       serialFrameCount++;
 
-//      pulseWidth[frame.sensor] = frame.width;
-
       if (pulseProcessorProcessPulse(&ppState, frame.sensor, frame.timestamp, frame.width, angles, &basestation, &axis)) {
       	// an angle was successfully measured
         frameCount++;
 
-				estimatePosition2(&ppState, angles);
-
-        if (basestation == 1 && axis == 1) { // 4 frames per cycle: BS0-AX0, BS0-AX1, BS1-AX0, BS1-AX1
-          cycleCount++;
-
-//          pulseProcessorApplyCalibration(&ppState, angles);
-//          estimatePosition(angles);
-
-//          for (uint8_t sensor = 0; sensor < PULSE_PROCESSOR_N_SENSORS; sensor++) {
-//            angles[sensor].validCount = 0;
-//          }
-        }
-
-//				estimatePosition2(&ppState, angles); //runs too often, will miss pulses
+				estimatePosition(&ppState, angles);
       }
 
       uint32_t nowMs = T2M(xTaskGetTickCount());
@@ -642,10 +554,7 @@ static const DeckDriver lighthouse_deck = {
 DECK_DRIVER(lighthouse_deck);
 
 LOG_GROUP_START(lighthouse)
-//LOG_ADD(LOG_FLOAT, rawAngle0x, &angles[0].angles[0][0])
-//LOG_ADD(LOG_FLOAT, rawAngle0y, &angles[0].angles[0][1])
-//LOG_ADD(LOG_FLOAT, rawAngle1x, &angles[0].angles[1][0])
-//LOG_ADD(LOG_FLOAT, rawAngle1y, &angles[0].angles[1][1])
+
 LOG_ADD(LOG_FLOAT, angle0x, &angles[0].correctedAngles[0][0])
 LOG_ADD(LOG_FLOAT, angle0y, &angles[0].correctedAngles[0][1])
 LOG_ADD(LOG_FLOAT, angle1x, &angles[0].correctedAngles[1][0])
@@ -669,49 +578,12 @@ LOG_ADD(LOG_FLOAT, angle1y_3, &angles[3].correctedAngles[1][1])
 LOG_ADD(LOG_FLOAT, x, &ext_pos.x)
 LOG_ADD(LOG_FLOAT, y, &ext_pos.y)
 LOG_ADD(LOG_FLOAT, z, &ext_pos.z)
-//LOG_ADD(LOG_FLOAT, x, &position[0])
-//LOG_ADD(LOG_FLOAT, y, &position[1])
-//LOG_ADD(LOG_FLOAT, z, &position[2])
-//LOG_ADD(LOG_FLOAT, delta, &delta)
-
-//LOG_ADD(LOG_FLOAT, posX_0, &positions[0][0])
-//LOG_ADD(LOG_FLOAT, posY_0, &positions[0][1])
-//LOG_ADD(LOG_FLOAT, posZ_0, &positions[0][2])
-//LOG_ADD(LOG_UINT8, posValid_0, &positionsIsValid[0])
-//
-//LOG_ADD(LOG_FLOAT, posX_1, &positions[1][0])
-//LOG_ADD(LOG_FLOAT, posY_1, &positions[1][1])
-//LOG_ADD(LOG_FLOAT, posZ_1, &positions[1][2])
-//LOG_ADD(LOG_UINT8, posValid_1, &positionsIsValid[1])
-//
-//LOG_ADD(LOG_FLOAT, posX_2, &positions[2][0])
-//LOG_ADD(LOG_FLOAT, posY_2, &positions[2][1])
-//LOG_ADD(LOG_FLOAT, posZ_2, &positions[2][2])
-//LOG_ADD(LOG_UINT8, posValid_2, &positionsIsValid[2])
-//
-//LOG_ADD(LOG_FLOAT, posX_3, &positions[3][0])
-//LOG_ADD(LOG_FLOAT, posY_3, &positions[3][1])
-//LOG_ADD(LOG_FLOAT, posZ_3, &positions[3][2])
-//LOG_ADD(LOG_UINT8, posValid_3, &positionsIsValid[3])
-
 LOG_ADD(LOG_FLOAT, delta, &deltaLog)
 
 LOG_ADD(LOG_FLOAT, serRt, &serialFrameRate)
 LOG_ADD(LOG_FLOAT, frmRt, &frameRate)
 LOG_ADD(LOG_FLOAT, cycleRt, &cycleRate)
 LOG_ADD(LOG_FLOAT, posRt, &positionRate)
-LOG_ADD(LOG_FLOAT, rayRt, &rayRate)
-
-//LOG_ADD(LOG_UINT16, width0, &pulseWidth[0])
-//#if PULSE_PROCESSOR_N_SENSORS > 1
-//LOG_ADD(LOG_UINT16, width1, &pulseWidth[1])
-//#endif
-//#if PULSE_PROCESSOR_N_SENSORS > 2
-//LOG_ADD(LOG_UINT16, width2, &pulseWidth[2])
-//#endif
-//#if PULSE_PROCESSOR_N_SENSORS > 3
-//LOG_ADD(LOG_UINT16, width3, &pulseWidth[3])
-//#endif
 
 LOG_ADD(LOG_UINT8, comSync, &comSynchronized)
 LOG_GROUP_STOP(lighthouse)
