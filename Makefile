64aa487eb56910e71b2e8ef18f3f4b8d1351028c
Arnaud Taffanel <arnaud@bitcraze.io>
2019-10-15 11:03:37 +0200

    closes #487: Implement app entry-point

--- Makefile
+++ Makefile
@@ -109,6 +109,16 @@ PROJ_OBJ += diskio_function_tests.o
 CFLAGS += -DUSD_RUN_DISKIO_FUNCTION_TESTS
 endif
 
+ifeq ($(APP), 1)
+CFLAGS += -DAPP_ENABLED=1
+endif
+ifdef APP_STACKSIZE
+CFLAGS += -DAPP_STACKSIZE=$(APP_STACKSIZE)
+endif
+ifdef APP_PRIORITY
+CFLAGS += -DAPP_PRIORITY=$(APP_PRIORITY)
+endif
+
 # Crazyflie sources
 VPATH += $(CRAZYFLIE_BASE)/src/init $(CRAZYFLIE_BASE)/src/hal/src $(CRAZYFLIE_BASE)/src/modules/src $(CRAZYFLIE_BASE)/src/utils/src $(CRAZYFLIE_BASE)/src/drivers/bosch/src $(CRAZYFLIE_BASE)/src/drivers/src $(CRAZYFLIE_BASE)/src/platform
 
@@ -150,7 +160,7 @@ PROJ_OBJ += vl53l1_register_funcs.o vl53l1_wait.o vl53l1_core_support.o
 PROJ_OBJ += system.o comm.o console.o pid.o crtpservice.o param.o
 PROJ_OBJ += log.o worker.o trigger.o sitaw.o queuemonitor.o msp.o
 PROJ_OBJ += platformservice.o sound_cf2.o extrx.o sysload.o mem_cf2.o
-PROJ_OBJ += range.o
+PROJ_OBJ += range.o app_handler.o
 
 # Stabilizer modules
 PROJ_OBJ += commander.o crtp_commander.o crtp_commander_rpyt.o
---
+++ src/modules/interface/app.h
@@ -0,0 +1,42 @@
+/*
+ *    ||          ____  _ __
+ * +------+      / __ )(_) /_______________ _____  ___
+ * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * LPS node firmware.
+ *
+ * Copyright 2019, Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Foobar is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/* app.h: App layer API */
+#pragma once
+
+/**
+ * App Inintialization
+ * 
+ * Called during firmware initialization. A default weak function is implemented
+ * that creates a task and calls appMain() when the system is started.
+ * 
+ * Implementation can be overwriten.
+ */
+void appInit();
+
+/**
+ * app main function, called when the Crazyflie has started from within a task created
+ * by appInit().
+ */
+void appMain();
