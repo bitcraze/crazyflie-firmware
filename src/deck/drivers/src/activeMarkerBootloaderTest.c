c925a506d0e9db94fba1924c10ff23af69a3b131
Arnaud Taffanel <arnaud@bitcraze.io>
2019-12-06 09:27:29 +0100

    #518: Add Active Marker bootloader factory test
    Not compiled by default, can be compiled and activated by adding to
    config.mk:
    ```
    CFLAGS += -DDECK_FORCE=activeMarkerBootloaderTest
    PROJ_OBJ += activeMarkerBootloaderTest.o
    ```

---
+++ src/deck/drivers/src/activeMarkerBootloaderTest.c
@@ -0,0 +1,106 @@
+/**
+ *    ||          ____  _ __                           
+ * +------+      / __ )(_) /_______________ _____  ___ 
+ * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
+ * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
+ *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
+ *
+ * Crazyflie control firmware
+ *
+ * Copyright (C) 2019 Bitcraze AB
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, in version 3.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * activeMarkerBootloaderTest.c: Production test for Active marker deck UART
+ *                               bootloader
+ */
+
+#include <stdbool.h>
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include "param.h"
+#include "debug.h"
+
+#include "deck.h"
+#include "uart1.h"
+
+#define DEBUG_MODULE "ActiveMarkerDeckBootTest"
+
+#include "log.h"
+
+static bool isInit = false;
+
+static bool trigger = false;
+static bool passed = false;
+
+static void task(void *param)
+{
+  while(1) {
+    if (trigger) {
+      trigger = false;
+
+      DEBUG_PRINT("Sending...\n");
+      uart1Putchar(0x7f);
+
+      uint8_t answer;
+      DEBUG_PRINT("Waiting...\n");
+      if (uart1GetDataWithTimout(&answer)) {
+        DEBUG_PRINT("Received %02x\n", (unsigned int)answer);
+
+        if (answer == 0x79) {
+          passed = true;
+        }
+      } else {
+        DEBUG_PRINT("Timeout!!!\n");
+      }
+      
+    }
+    vTaskDelay(M2T(100));
+  }
+  
+}
+
+static void init(DeckInfo *info)
+{
+  if (isInit) {
+    return;
+  }
+
+  DEBUG_PRINT("Initializing Active Marker deck bootloader test driver...\n");
+
+  uart1InitWithParity(9600, uart1ParityEven);
+
+  xTaskCreate(task, "amarkBootTest",
+              configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+
+  isInit = true;
+}
+
+static const DeckDriver deckDriver = {
+  .name="activeMarkerBootloaderTest",
+
+  .init = init,
+};
+
+DECK_DRIVER(deckDriver);
+
+
+PARAM_GROUP_START(amarkBootTest)
+PARAM_ADD(PARAM_UINT8, trigger, &trigger)
+PARAM_GROUP_STOP(amarkBootTest)
+
+LOG_GROUP_START(amarkBootTest)
+LOG_ADD(LOG_UINT8, passed, &passed)
+LOG_GROUP_STOP(amarkBootTest)
