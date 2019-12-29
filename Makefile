199e558ced6b7bb475177ce8a7140447724b57c1
Kristoffer Richardsson <kristoffer@bitcraze.io>
2019-12-29 14:27:26 +0100

    #517 Refactoring

--- Makefile
+++ Makefile
@@ -159,7 +159,7 @@ PROJ_OBJ += vl53l1_register_funcs.o vl53l1_wait.o vl53l1_core_support.o
 # Modules
 PROJ_OBJ += system.o comm.o console.o pid.o crtpservice.o param.o
 PROJ_OBJ += log.o worker.o trigger.o sitaw.o queuemonitor.o msp.o
-PROJ_OBJ += platformservice.o sound_cf2.o extrx.o sysload.o mem_cf2.o
+PROJ_OBJ += platformservice.o sound_cf2.o extrx.o sysload.o mem.o
 PROJ_OBJ += range.o app_handler.o
 
 # Stabilizer modules
--- src/modules/interface/mem.h
+++ src/modules/interface/mem.h
@@ -1,13 +1,13 @@
 /**
- *    ||          ____  _ __                           
- * +------+      / __ )(_) /_______________ _____  ___ 
+ *    ||          ____  _ __
+ * +------+      / __ )(_) /_______________ _____  ___
  * | 0xBC |     / __  / / __/ ___/ ___/ __ `/_  / / _ \
  * +------+    / /_/ / / /_/ /__/ /  / /_/ / / /_/  __/
  *  ||  ||    /_____/_/\__/\___/_/   \__,_/ /___/\___/
  *
  * Crazyflie control firmware
  *
- * Copyright (C) 2012 BitCraze AB
+ * Copyright (C) 2012-2019 BitCraze AB
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,23 +21,13 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
- * log.h - Dynamic log system
+ * mem.h - Memory sub system
  */
 
 #ifndef __MEM_H__
 #define __MEM_H__
 
 #include <stdbool.h>
-#include <stdint.h>
-
-
-#define MEM_SETTINGS_CH     0
-#define MEM_READ_CH         1
-#define MEM_WRITE_CH        2
-
-#define MEM_CMD_GET_NBR     1
-#define MEM_CMD_GET_INFO    2
-
 
 /* Public functions */
 void memInit(void);
