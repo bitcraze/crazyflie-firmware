bitcraze/crazyflie-firmware

commit bf8a584bf6c05b6ee4a3d1f61f05e0c56147b3e7

#495 Refactoring. Added all task priorities and names as defines in config.h

src/deck/drivers/src/bigquad.c
@@ -90,8 +90,8 @@ static void bigquadInit(DeckInfo *info)
 #ifdef BQ_DECK_ENABLE_OSD
   uart1Init(115200);
   mspInit(&s_MspObject, osdResponseCallback);
-  xTaskCreate(osdTask, "BQ_OSDTASK",
-              configMINIMAL_STACK_SIZE, NULL, /*priority*/1, NULL);
+  xTaskCreate(osdTask, BQ_OSD_TASK_NAME,
+              configMINIMAL_STACK_SIZE, NULL, BQ_DECK_TASK_PRI, NULL);
 #endif
 
   isInit = true;

src/config/config.h
@@ -85,6 +85,13 @@
 #define USDWRITE_TASK_PRI       0
 #define PCA9685_TASK_PRI        3
 #define CMD_HIGH_LEVEL_TASK_PRI 2
+#define BQ_OSD_TASK_PRI         1
+#define GTGPS_DECK_TASK_PRI     1
+#define LIGHTHOUSE_TASK_PRI     1
+#define LPS_DECK_TASK_PRI       5
+#define OA_DECK_TASK_PRI        3
+#define UART1_TEST_TASK_PRI     1
+#define UART2_TEST_TASK_PRI     1
 
 #define SYSLINK_TASK_PRI        3
 #define USBLINK_TASK_PRI        3
@@ -123,6 +130,13 @@
 #define PCA9685_TASK_NAME       "PCA9685"
 #define CMD_HIGH_LEVEL_TASK_NAME "CMDHL"
 #define MULTIRANGER_TASK_NAME   "MR"
+#define BQ_OSD_TASK_NAME        "BQ_OSDTASK"
+#define GTGPS_DECK_TASK_NAME    "GTGPS"
+#define LIGHTHOUSE_TASK_NAME    "LH"
+#define LPS_DECK_TASK_NAME      "LPS"
+#define OA_DECK_TASK_NAME       "OA"
+#define UART1_TEST_TASK_NAME    "UART1TEST"
+#define UART2_TEST_TASK_NAME    "UART2TEST"
 
 //Task stack sizes
 #define SYSTEM_TASK_STACKSIZE         (2* configMINIMAL_STACK_SIZE)

src/deck/drivers/src/gtgps.c
@@ -282,8 +282,8 @@ static void gtgpsInit(DeckInfo *info)
   DEBUG_PRINT("Enabling reading from GlobalTop GPS\n");
   uart1Init(9600);
 
-  xTaskCreate(gtgpsTask, "GTGPS",
-              configMINIMAL_STACK_SIZE, NULL, /*priority*/1, NULL);
+  xTaskCreate(gtgpsTask, GTGPS_DECK_TASK_NAME,
+              configMINIMAL_STACK_SIZE, NULL, GTGPS_DECK_TASK_PRI, NULL);
 
   isInit = true;
 }

src/deck/drivers/src/lighthouse.c
@@ -408,8 +408,8 @@ static void lighthouseInit(DeckInfo *info)
   uart1Init(230400);
   lhblInit(I2C1_DEV);
 
-  xTaskCreate(lighthouseTask, "LH",
-              2*configMINIMAL_STACK_SIZE, NULL, /*priority*/1, NULL);
+  xTaskCreate(lighthouseTask, LIGHTHOUSE_TASK_NAME,
+              2*configMINIMAL_STACK_SIZE, NULL, LIGHTHOUSE_TASK_PRI, NULL);
 
   isInit = true;
 }

src/deck/drivers/src/locodeck.c
@@ -469,8 +469,8 @@ static void dwm1000Init(DeckInfo *info)
   vSemaphoreCreateBinary(irqSemaphore);
   algoSemaphore= xSemaphoreCreateMutex();
 
-  xTaskCreate(uwbTask, "lps", 3*configMINIMAL_STACK_SIZE, NULL,
-                    5/*priority*/, NULL);
+  xTaskCreate(uwbTask, LPS_DECK_TASK_NAME, 3*configMINIMAL_STACK_SIZE, NULL,
+                    LPS_DECK_TASK_PRI, NULL);
 
   isInit = true;
 }

src/deck/drivers/src/oa.c
@@ -107,8 +107,8 @@ static void oaInit()
 
   isInit = true;
 
-  xTaskCreate(oaTask, "oa", 2*configMINIMAL_STACK_SIZE, NULL,
-              /*priority*/3, NULL);
+  xTaskCreate(oaTask, OA_DECK_TASK_NAME, 2*configMINIMAL_STACK_SIZE, NULL,
+              OA_DECK_TASK_PRI, NULL);
 }
 
 static bool oaTest()

src/deck/drivers/src/test/uart1test.c
@@ -50,7 +50,7 @@ static void uart1testInit(DeckInfo *info)
 
   uart1Init(115200);
 
-  xTaskCreate(uart1testTask, "UART1TEST", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+  xTaskCreate(uart1testTask, UART1_TEST_TASK_NAME, configMINIMAL_STACK_SIZE, NULL, UART1_TEST_TASK_PRI, NULL);
 
   isInit = true;
 }

src/deck/drivers/src/test/uart2test.c
@@ -50,7 +50,7 @@ static void uart2testInit(DeckInfo *info)
 
   uart2Init(115200);
 
-  xTaskCreate(uart2testTask, "UART2TEST", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+  xTaskCreate(uart2testTask, UART2_TEST_TASK_NAME, configMINIMAL_STACK_SIZE, NULL, UART2_TEST_TASK_PRI, NULL);
 
   isInit = true;
 }

commit 25bc29c3e550e72d67e32138cc32a5a88753e893

Fixing a typo in bigquad.c introduced by #495 Refactoring...
Fixing a typo introduced in 
https://github.com/bitcraze/crazyflie-firmware/commit/bf8a584bf6c05b6ee4a3d1f61f05e0c56147b3e7

src/deck/drivers/src/bigquad.c
@@ -91,7 +91,7 @@ static void bigquadInit(DeckInfo *info)
   uart1Init(115200);
   mspInit(&s_MspObject, osdResponseCallback);
   xTaskCreate(osdTask, BQ_OSD_TASK_NAME,
-              configMINIMAL_STACK_SIZE, NULL, BQ_DECK_TASK_PRI, NULL);
+              configMINIMAL_STACK_SIZE, NULL, BQ_OSD_TASK_PRI, NULL);
 #endif
 
   isInit = true;