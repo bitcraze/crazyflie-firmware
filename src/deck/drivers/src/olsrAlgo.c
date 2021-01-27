9e20c542ea13e95ee6cdcea3156056dfb092f967
HuoHaodong <inf1nityhhd@163.com>
2021-01-27 20:58:36 +0800

    add ts process method

--- src/deck/drivers/src/olsrAlgo.c
+++ src/deck/drivers/src/olsrAlgo.c
@@ -152,7 +152,7 @@ void olsrRxCallback(dwDevice_t *dev){
 
 //packet process
 
-void olsr_ts_process(const olsrMessage_t* ts_msg){
+void olsrProcessTs(const olsrMessage_t* tsMsg){
 
 }
 
@@ -1078,7 +1078,7 @@ void olsrPacketDispatch(const packet_t* rxPacket)
                 break;
             case TS_MESSAGE:
                 DEBUG_PRINT_OLSR_RECEIVE("TS_MESSAGE\n");
-                //olsr_ts_process(olsr_message);
+                olsrProcessTs((olsrMessage_t*)olsr_message);
                 break;
             default:
                 DEBUG_PRINT_OLSR_RECEIVE("WRONG MESSAGE\n");
