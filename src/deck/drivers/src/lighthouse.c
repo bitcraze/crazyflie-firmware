7850327c2e2bd7567545b65d70b1d5e655a6d3b8
Arnaud Taffanel <arnaud@bitcraze.se>
2021-02-02 10:01:30 +0100

    Fixes #678: Fix lighthouse init param spelling

--- src/deck/drivers/src/lighthouse.c
+++ src/deck/drivers/src/lighthouse.c
@@ -74,5 +74,5 @@ static const DeckDriver lighthouse_deck = {
 DECK_DRIVER(lighthouse_deck);
 
 PARAM_GROUP_START(deck)
-PARAM_ADD(PARAM_UINT8 | PARAM_RONLY, bdLighthouse4, &isInit)
+PARAM_ADD(PARAM_UINT8 | PARAM_RONLY, bcLighthouse4, &isInit)
 PARAM_GROUP_STOP(deck)
