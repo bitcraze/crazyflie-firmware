103192251f3f12c2128e5495a8a406c8695853e1
Kristoffer Richardsson <kristoffer@bitcraze.io>
2021-02-18 14:04:25 +0100

    #694 Use more functionality from the lib

--- tools/lighthouse/get_bs_geometry.py
+++ tools/lighthouse/get_bs_geometry.py
@@ -42,158 +42,100 @@
 
 import argparse
 import logging
-import math
-import time
 import numpy as np
-import cv2 as cv
-from threading import Semaphore
-import sys
+from threading import Event
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.mem import LighthouseBsGeometry
-from cflib.crazyflie.mem import MemoryElement
+from cflib.crazyflie.mem import LighthouseMemHelper
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
-from cflib.localization.lighthouse_bs_geo import LighthouseBsGeoEstimator
-from cflib.localization.lighthouse_bs_vector import LighthouseBsVector
+from cflib.localization import LighthouseBsGeoEstimator
+from cflib.localization import LighthouseSweepAngleAverageReader
 
 
-# Number of sample to accumulate for the geometry calculation
-N_SAMPLE = 50
+class Estimator:
+    def __init__(self):
+        self.sensor_vectors_all = None
+        self.collection_event = Event()
+        self.write_event = Event()
 
+    def angles_collected_cb(self, angles):
+        self.sensor_vectors_all = angles
+        self.collection_event.set()
 
-def read_sensors(scf: SyncCrazyflie):
-    # Mutext released by the callback function when enough measurements have
-    # been accumulated
-    reading_finished = Semaphore(0)
+    def write_done_cb(self, success):
+        if not success:
+            print("Write to CF failed!")
+        self.write_event.set()
 
-    measurements = {}
+    def estimate(self, uri, do_write):
+        cf = Crazyflie(rw_cache='./cache')
+        with SyncCrazyflie(uri, cf=cf) as scf:
+            print("Reading sensor data...")
+            sweep_angle_reader = LighthouseSweepAngleAverageReader(scf.cf, self.angles_collected_cb)
+            sweep_angle_reader.start_angle_collection()
+            self.collection_event.wait()
 
-    # Callback accumulating measurements
-    def _loc_callback(pk):
-        if pk.type != scf.cf.loc.LH_ANGLE_STREAM:
-            return
+            print("Estimating position of base stations...")
+            geometries = {}
+            estimator = LighthouseBsGeoEstimator()
+            for id in sorted(self.sensor_vectors_all.keys()):
+                average_data = self.sensor_vectors_all[id]
+                sensor_data = average_data[1]
+                rotation_bs_matrix, position_bs_vector = estimator.estimate_geometry(sensor_data)
+                is_valid = estimator.sanity_check_result(position_bs_vector)
+                if is_valid:
+                    geo = LighthouseBsGeometry()
+                    geo.rotation_matrix = rotation_bs_matrix
+                    geo.origin = position_bs_vector
+                    geo.valid = True
 
-        print(pk.data["basestation"], end='')
-        sys.stdout.flush()
+                    geometries[id] = geo
 
-        if not pk.data["basestation"] in measurements:
-            measurements[pk.data["basestation"]] = {
-                "count": 1,
-                "sensors": [
-                    pk.data["x"],
-                    pk.data["y"]
-                ]
-            }
-        else:
-            for axis, axis_name in enumerate(['x', 'y']):
-                for sensor in range(4):
-                    measurements[pk.data["basestation"]]["sensors"][axis][sensor] += \
-                        pk.data[axis_name][sensor]
+                    self.print_geo(rotation_bs_matrix, position_bs_vector, is_valid)
+                else:
+                    print("Warning: could not find valid solution for " + id + 1)
 
-            measurements[pk.data["basestation"]]["count"] += 1
-
-            if measurements[pk.data["basestation"]]["count"] > N_SAMPLE:
                 print()
-                reading_finished.release()
 
-    # Setup callback function and start streaming
-    scf.cf.loc.receivedLocationPacket.add_callback(_loc_callback)
-    scf.cf.param.set_value("locSrv.enLhAngleStream", 1)
+            if do_write:
+                print("Uploading geo data to CF")
+                helper = LighthouseMemHelper(scf.cf)
+                helper.write_geos(geometries, self.write_done_cb)
+                self.write_event.wait()
 
-    # Wait for enough sample to be acquired
-    reading_finished.acquire()
+    def print_geo(self, rotation_cf, position_cf, is_valid):
+        print('C-format')
+        if is_valid:
+            valid_c = 'true'
+        else:
+            valid_c = 'false'
 
-    # Stop streaming and disable callback function
-    scf.cf.param.set_value("locSrv.enLhAngleStream", 0)
-    scf.cf.loc.receivedLocationPacket.remove_callback(_loc_callback)
+        print('{.valid = ' + valid_c + ', .origin = {', end='')
+        for i in position_cf:
+            print("{:0.6f}, ".format(i), end='')
 
-    # Average the sum of measurements and generate t
-    for basestation in measurements:
-        for axis, axis_name in enumerate(['x', 'y']):
-            for sensor in range(4):
-                measurements[basestation]["sensors"][axis][sensor] /= \
-                    measurements[basestation]["count"]
+        print("}, .mat = {", end='')
 
-    # Reorganize data to be used by the geometry functions
-    sensor_vectors = {}
-    for bs in measurements:
-        sensor_vectors[bs] = ([None, None, None, None])
-        for sensor in range(4):
-            horiz_angle = measurements[bs]["sensors"][0][sensor]
-            vertical_angle = measurements[bs]["sensors"][1][sensor]
-            bs_vector = LighthouseBsVector(horiz_angle, vertical_angle)
-            sensor_vectors[bs][sensor] = bs_vector
+        for i in rotation_cf:
+            print("{", end='')
+            for j in i:
+                print("{:0.6f}, ".format(j), end='')
+            print("}, ", end='')
 
-    return sensor_vectors
+        print("}},")
 
-def print_geo(rotation_cf, position_cf, is_valid):
-    print('C-format')
-    if is_valid:
-        valid_c = 'true'
-    else:
-        valid_c = 'false'
-
-    print('{.valid = ' + valid_c + ', .origin = {', end='')
-    for i in position_cf:
-        print("{:0.6f}, ".format(i), end='')
-
-    print("}, .mat = {", end='')
-
-    for i in rotation_cf:
-        print("{", end='')
-        for j in i:
-            print("{:0.6f}, ".format(j), end='')
-        print("}, ", end='')
-
-    print("}},")
-
-    print()
-    print('python-format')
-    print('geo = LighthouseBsGeometry()')
-    print('geo.origin =', np.array2string(position_cf, separator=','))
-    print('geo.rotation_matrix = [', end='')
-    for row in rotation_cf:
-        print(np.array2string(row, separator=','), end='')
-        print(', ', end='')
-    print(']')
-    print('geo.valid =', is_valid)
-
-
-class WriteMem:
-    def __init__(self, scf, bs, geo):
-        mems = scf.cf.mem.get_mems(MemoryElement.TYPE_LH)
-
-        count = len(mems)
-        if count != 1:
-            raise Exception('Unexpected nr of memories found:', count)
-
-        self.data_written = False
-        mems[0].write_geo_data(bs, geo, self._data_written,
-                                write_failed_cb=self._write_failed)
-
-        while not self.data_written:
-            time.sleep(0.1)
-
-    def _data_written(self, mem, addr):
-        self.data_written = True
-        print('Data written')
-
-    def _write_failed(self, mem, addr):
-        raise Exception("Write failed")
-
-
-def upload_geo_data(scf, geometries):
-    for bs, data in geometries.items():
-        geo = LighthouseBsGeometry()
-        geo.rotation_matrix = data[0]
-        geo.origin = data[1]
-        geo.valid = data[2]
-
-        WriteMem(scf, bs, geo)
-
-
-##################################################
+        print()
+        print('python-format')
+        print('geo = LighthouseBsGeometry()')
+        print('geo.origin =', np.array2string(position_cf, separator=','))
+        print('geo.rotation_matrix = [', end='')
+        for row in rotation_cf:
+            print(np.array2string(row, separator=','), end='')
+            print(', ', end='')
+        print(']')
+        print('geo.valid =', is_valid)
 
 
 parser = argparse.ArgumentParser()
@@ -208,30 +150,5 @@ if args.uri:
 logging.basicConfig(level=logging.ERROR)
 cflib.crtp.init_drivers(enable_debug_driver=False)
 
-cf = Crazyflie(rw_cache='./cache')
-with SyncCrazyflie(uri, cf=cf) as scf:
-    print("Reading sensor data...")
-    sensor_vectors_all = read_sensors(scf)
-    print("Estimating position of base stations...")
-
-    geometries = {}
-    estimator = LighthouseBsGeoEstimator()
-    for bs in sorted(sensor_vectors_all.keys()):
-        sensor_vectors = sensor_vectors_all[bs]
-        print("Base station ", bs)
-
-        rotation_cf, position_cf = estimator.estimate_geometry(sensor_vectors)
-
-        is_valid = estimator.sanity_check_result(position_cf)
-        if not is_valid:
-            position_cf = np.array([0, 0, 0])
-            rotation_cf = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
-            print("Warning: could not find valid solution")
-
-        print_geo(rotation_cf, position_cf, is_valid)
-        geometries[bs] = [rotation_cf, position_cf, is_valid]
-        print()
-
-    if args.write:
-        print("Uploading geo data")
-        upload_geo_data(scf, geometries)
+estimator = Estimator()
+estimator.estimate(uri, args.write)
