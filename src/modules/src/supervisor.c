f0532ddb7f06c194b2211d0267b806b71efb2ace
Wolfgang Hoenig <whoenig@caltech.edu>
2021-03-19 10:42:24 +0100

    avoid compiler error for unused isTumbledCheck function

--- src/modules/src/supervisor.c
+++ src/modules/src/supervisor.c
@@ -116,7 +116,7 @@ void supervisorUpdate(const sensorData_t *data)
 {
   isFlying = isFlyingCheck();
 
-  // isTumbled = isTumbledCheck(data);
+  isTumbled = isTumbledCheck(data);
   isTumbled = false;
   if (isTumbled && isFlying) {
     stabilizerSetEmergencyStop();
