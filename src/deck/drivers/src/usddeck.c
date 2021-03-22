bitcraze/crazyflie-firmware

commit 303f0736298d7ec7151ac23ebb8bb9599e019334

Event trigger accuracy improvements. (#734)
* introduces version 2 of the recorded binary file, which uses a
  microsecond timestamp, rather than a millisecond timestamp. Older
  version-1 files can still be decoded in the same way as before.
* introduces usd.canLog readonly parameter to check if uSD card is
  inserted and configuration file is valid.
* support for more payload variables for event triggers.
* reduction of data loss when starting/stopping logging.

src/deck/drivers/src/usddeck.c
@@ -457,7 +457,11 @@ static void usdInit(DeckInfo *info)
 
 static void usddeckWriteEventData(const usdLogEventConfig_t* cfg, const uint8_t* payload, uint8_t payloadSize)
 {
-  uint32_t ticks = xTaskGetTickCount();
+  uint64_t ticks = usecTimestamp();
+
+  if (!enableLogging) {
+    return;
+  }
 
   ++usdLogStats.eventsRequested;
 
@@ -475,7 +479,7 @@ static void usddeckWriteEventData(const usdLogEventConfig_t* cfg, const uint8_t*
     /* write data into buffer */
     uint16_t event_id = cfg->eventId;
     ringBuffer_push(&logBuffer, &event_id, sizeof(event_id));
-    ringBuffer_push(&logBuffer, &ticks, 4);
+    ringBuffer_push(&logBuffer, &ticks, sizeof(ticks));
     if (payloadSize) {
       ringBuffer_push(&logBuffer, payload, payloadSize);
     }
@@ -675,7 +679,7 @@ static void usdLogTask(void* prm)
         vTaskResume(xHandleWriteTask);
       }
 
-      if (enableLogging && usdLogConfig.mode == usddeckLoggingMode_Asyncronous) {
+      if (enableLogging && usdLogConfig.mode == usddeckLoggingMode_Asynchronous) {
         usddeckTriggerLogging();
       }
       lastEnableLogging = enableLogging;
@@ -816,7 +820,7 @@ static void usdWriteTask(void* prm)
         uint8_t magic = 0xBC;
         usdWriteData(&magic, sizeof(magic));
         
-        uint16_t version = 1;
+        uint16_t version = 2;
         usdWriteData(&version, sizeof(version));
 
         uint16_t numEventTypes = usdLogConfig.numEventConfigs;
@@ -931,6 +935,21 @@ static void usdWriteTask(void* prm)
             xSemaphoreGive(logBufferMutex);
           }
         }
+        // write everything that's still in the buffer
+        xSemaphoreTake(logBufferMutex, portMAX_DELAY);
+        while (true) {
+          const uint8_t *buf;
+          uint16_t size;
+          bool hasData = ringBuffer_pop_start(&logBuffer, &buf, &size);
+          if (hasData) {
+            usdWriteData(buf, size);
+            ringBuffer_pop_done(&logBuffer);
+          } else {
+            break;
+          }
+        }
+        xSemaphoreGive(logBufferMutex);
+
         // write CRC
         uint32_t crcValue = crc32Out(&crcContext);
         usdWriteData(&crcValue, sizeof(crcValue));
@@ -1007,6 +1026,7 @@ PARAM_ADD(PARAM_UINT8 | PARAM_RONLY, bcUSD, &isInit)
 PARAM_GROUP_STOP(deck)
 
 PARAM_GROUP_START(usd)
+PARAM_ADD(PARAM_UINT8 | PARAM_RONLY, canLog, &initSuccess)
 PARAM_ADD(PARAM_UINT8, logging, &enableLogging) /* use to start/stop logging*/
 PARAM_GROUP_STOP(usd)
 

src/deck/drivers/interface/usddeck.h
@@ -8,7 +8,7 @@ enum usddeckLoggingMode_e
 {
   usddeckLoggingMode_Disabled = 0,
   usddeckLoggingMode_SynchronousStabilizer,
-  usddeckLoggingMode_Asyncronous,
+  usddeckLoggingMode_Asynchronous,
 };
 
 // returns true if logging is enabled

src/modules/interface/eventtrigger.h
@@ -84,26 +84,28 @@ static const eventtrigger myEventTrigger = {
 
 /* The same code above can be generated using the following macro:
 
-EVENTTRIGGER(myEvent, UINT8, var1, UINT32, var2)
+EVENTTRIGGER(myEvent, uint8, var1, uint32, var2)
 
 To debug/develop the macros, a good way is to create a new file "etdbg.c" with the following content
 and then execute "gcc -E etdbg.c":
 
 #include "src/modules/interface/eventtrigger.h"
-EVENTTRIGGER(myEvent, UINT8, var1, UINT32, var2)
+EVENTTRIGGER(myEvent, uint8, var1, uint32, var2)
 */
 
 #ifndef UNIT_TEST_MODE
 
 /* Macro magic, see https://codecraft.co/2014/11/25/variadic-macros-tricks/ */
-#define _GET_NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, N, ...) N
+#define _GET_NTH_ARG(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, N, ...) N
 #define _fe_0(_call, ...)
 #define _fe_2(_call, k, v, ...) _call(k, v) _fe_0(_call, __VA_ARGS__)
 #define _fe_4(_call, k, v, ...) _call(k, v) _fe_2(_call, __VA_ARGS__)
 #define _fe_6(_call, k, v, ...) _call(k, v) _fe_4(_call, __VA_ARGS__)
 #define _fe_8(_call, k, v, ...) _call(k, v) _fe_6(_call, __VA_ARGS__)
+#define _fe_10(_call, k, v, ...) _call(k, v) _fe_8(_call, __VA_ARGS__)
 #define CALL_MACRO_FOR_EACH_PAIR(x, ...)                \
     _GET_NTH_ARG("ignored", ##__VA_ARGS__,              \
+    _fe_10, _invalid_,                                   \
     _fe_8, _invalid_,                                   \
     _fe_6, _invalid_,                                   \
     _fe_4, _invalid_,                                   \
@@ -112,6 +114,7 @@ EVENTTRIGGER(myEvent, UINT8, var1, UINT32, var2)
     (x, ##__VA_ARGS__)
 #define CALL_MACRO_IF_EMPTY(TRUE_MACRO, FALSE_MACRO, NAME, ...) \
     _GET_NTH_ARG("ignored", ##__VA_ARGS__,                      \
+        TRUE_MACRO, _invalid_,                                  \
         TRUE_MACRO, _invalid_,                                  \
         TRUE_MACRO, _invalid_,                                  \
         TRUE_MACRO, _invalid_,                                  \

tools/usdlog/cfusdlog.py
@@ -31,8 +31,8 @@ def decode(filename):
 
     # check version
     version, num_event_types = struct.unpack('HH', data[1:5])
-    if version != 1:
-        print("Unsupported version!")
+    if version != 1 and version != 2:
+        print("Unsupported version!", version)
         return
 
     result = dict()
@@ -65,8 +65,13 @@ def decode(filename):
             }
 
     while idx < len(data) - 4:
-        event_id, timestamp, = struct.unpack('<HI', data[idx:idx+6])
-        idx += 6
+        if version == 1:
+            event_id, timestamp, = struct.unpack('<HI', data[idx:idx+6])
+            idx += 6
+        elif version == 2:
+            event_id, timestamp, = struct.unpack('<HQ', data[idx:idx+10])
+            timestamp = timestamp / 1000.0
+            idx += 10
         event = event_by_id[event_id]
         fmtStr = event['fmtStr']
         eventData = struct.unpack(fmtStr, data[idx:idx+event['numBytes']])

commit fdc87523a77063cb99561f4a05c19b8e80466901

Fix regression that caused invalid data in uSD card log files.
This regression was introduced with 303f0736298d7ec7151ac23ebb8bb9599e019334,
where the timestamp was changed to 64bit (8 Bytes), but the dataSize
check remained for 4 bytes. Some of the ringBuffer_push calls can fail
in that case, causing invalid events in the buffer.

This fix corrects the dataSize computation from 4 bytes to sizeof(ticks)

src/deck/drivers/src/usddeck.c
@@ -472,7 +472,7 @@ static void usddeckWriteEventData(const usdLogEventConfig_t* cfg, const uint8_t*
     vTaskResume(xHandleWriteTask);
   }
 
-  int dataSize = 2 + 4 + payloadSize + cfg->numBytes;
+  int dataSize = sizeof(cfg->eventId) + sizeof(ticks) + payloadSize + cfg->numBytes;
 
   // only write if we have enough space
   if (ringBuffer_availableSpace(&logBuffer) >= dataSize) {