410cfa72fe6bb907536263057c181b7929c4b8d5
Tobias Antonson <tobbeanton@gmail.com>
2021-10-07 14:35:22 +0200

    Revert "elf-sanity: Add PARAM_CALLBACK awareness"
    This reverts commit 04f7726ed8697234ab6e10a2e33efe07bb61c920.

--- tools/verify/elf_sanity.py
+++ tools/verify/elf_sanity.py
@@ -39,10 +39,7 @@ def param_type_to_str(t: int) -> str:
     if t & (1 << 6):  # PARAM_RONLY set
         extra += ' | PARAM_RONLY'
 
-    if t & (1 << 4):  # PARAM_CALLBACK set
-        extra += ' | PARAM_CALLBACK'
-
-    int_type = t & ~(1 << 5 | 1 << 6 | 1 << 4)
+    int_type = t & ~(1 << 5 | 1 << 6)
 
     return '{:12}{}'.format(param_type_to_str_dict[int_type], extra)
 
