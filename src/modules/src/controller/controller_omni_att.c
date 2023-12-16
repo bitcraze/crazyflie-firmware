a3d344e2691ff3161bcc948c930b5374bc66dafd
SFWen2 <SFWen@itri.org.tw>
2023-12-15 16:46:43 -0800

    1215

--- src/modules/src/controller/controller_omni_att.c
+++ src/modules/src/controller/controller_omni_att.c
@@ -74,55 +74,47 @@ void controllerOmniAtt(control_t *control, const setpoint_t *setpoint,
 // log/param name can't be too long, otherwise error
 // only one log group is allowed
 
-// LOG_GROUP_START(sctrl_ppid)
+LOG_GROUP_START(sctrl_omni)
 
-// LOG_ADD(LOG_FLOAT, e_alpha, &single_qc_ppid_Y.error_alpha)
-// LOG_ADD(LOG_FLOAT, e_beta, &single_qc_ppid_Y.error_beta)
-// LOG_ADD(LOG_FLOAT, e_alphas, &single_qc_ppid_Y.error_alphas)
-// LOG_ADD(LOG_FLOAT, e_betas, &single_qc_ppid_Y.error_betas)
-// LOG_ADD(LOG_FLOAT, u_alpha, &single_qc_ppid_Y.u_alpha)
-// LOG_ADD(LOG_FLOAT, u_beta, &single_qc_ppid_Y.u_beta)
+LOG_ADD(LOG_FLOAT, qw_r, &omni_attitude_controller_U.qw_r)
+LOG_ADD(LOG_FLOAT, qx_r, &omni_attitude_controller_U.qx_r)
+LOG_ADD(LOG_FLOAT, qy_r, &omni_attitude_controller_U.qy_r)
+LOG_ADD(LOG_FLOAT, qz_r, &omni_attitude_controller_U.qz_r)
 
-// LOG_ADD(LOG_FLOAT, t_be, &single_qc_ppid_Y.t_betae)
-// LOG_ADD(LOG_FLOAT, t_bin, &single_qc_ppid_Y.t_betain)
-// LOG_ADD(LOG_FLOAT, t_ae, &single_qc_ppid_Y.t_alphae)
-// LOG_ADD(LOG_FLOAT, t_ain, &single_qc_ppid_Y.t_alphain)
+LOG_ADD(LOG_FLOAT, wx_r, &omni_attitude_controller_U.wx_r)
+LOG_ADD(LOG_FLOAT, wy_r, &omni_attitude_controller_U.wy_r)
+LOG_ADD(LOG_FLOAT, wz_r, &omni_attitude_controller_U.wz_r)
 
-// LOG_ADD(LOG_FLOAT, x_gyro, &single_qc_ppid_U.omega_x)
-// LOG_ADD(LOG_FLOAT, b_gyro, &single_qc_ppid_U.beta_speed)
-// LOG_ADD(LOG_FLOAT, z_gyro, &single_qc_ppid_U.omega_z)
+LOG_ADD(LOG_FLOAT, qw_IMU, &omni_attitude_controller_U.qw_IMU)
+LOG_ADD(LOG_FLOAT, qx_IMU, &omni_attitude_controller_U.qx_IMU)
+LOG_ADD(LOG_FLOAT, qy_IMU, &omni_attitude_controller_U.qy_IMU)
+LOG_ADD(LOG_FLOAT, qz_IMU, &omni_attitude_controller_U.qz_IMU)
 
-// LOG_ADD(LOG_FLOAT, t_m1, &single_qc_ppid_Y.t_m1)
-// LOG_ADD(LOG_FLOAT, t_m2, &single_qc_ppid_Y.t_m2)
-// LOG_ADD(LOG_FLOAT, t_m3, &single_qc_ppid_Y.t_m3)
-// LOG_ADD(LOG_FLOAT, t_m4, &single_qc_ppid_Y.t_m4)
+LOG_ADD(LOG_FLOAT, gyro_x, &omni_attitude_controller_U.gyro_x)
+LOG_ADD(LOG_FLOAT, gyro_y, &omni_attitude_controller_U.gyro_y)
+LOG_ADD(LOG_FLOAT, gyro_z, &omni_attitude_controller_U.gyro_z)
 
-// LOG_GROUP_STOP(sctrl_ppid)
+LOG_ADD(LOG_FLOAT, t_m1, &omni_attitude_controller_Y.t_m1)
+LOG_ADD(LOG_FLOAT, t_m2, &omni_attitude_controller_Y.t_m2)
+LOG_ADD(LOG_FLOAT, t_m3, &omni_attitude_controller_Y.t_m3)
+LOG_ADD(LOG_FLOAT, t_m4, &omni_attitude_controller_Y.t_m4)
 
+LOG_GROUP_STOP(sctrl_omni)
 
+PARAM_GROUP_START(sparam_omni)
+PARAM_ADD(PARAM_FLOAT, KRx, &omni_attitude_controller_P.KR[0])
+PARAM_ADD(PARAM_FLOAT, KRy, &omni_attitude_controller_P.KR[4])
+PARAM_ADD(PARAM_FLOAT, KRz, &omni_attitude_controller_P.KR[8])
 
+PARAM_ADD(PARAM_FLOAT, Kix, &omni_attitude_controller_P.Ki[0])
+PARAM_ADD(PARAM_FLOAT, Kiy, &omni_attitude_controller_P.Ki[4])
+PARAM_ADD(PARAM_FLOAT, Kiz, &omni_attitude_controller_P.Ki[8])
 
-// PARAM_GROUP_START(sparam_ppid)
-// PARAM_ADD(PARAM_FLOAT, pgaina, &single_qc_ppid_P.pgaina)
-// PARAM_ADD(PARAM_FLOAT, igaina, &single_qc_ppid_P.igaina)
-// PARAM_ADD(PARAM_FLOAT, dgaina, &single_qc_ppid_P.dgaina)
+PARAM_ADD(PARAM_FLOAT, Kwx, &omni_attitude_controller_P.Kw[0])
+PARAM_ADD(PARAM_FLOAT, Kwy, &omni_attitude_controller_P.Kw[4])
+PARAM_ADD(PARAM_FLOAT, Kwz, &omni_attitude_controller_P.Kw[8])
 
-// PARAM_ADD(PARAM_FLOAT, pgainb, &single_qc_ppid_P.pgainb)
-// PARAM_ADD(PARAM_FLOAT, igainb, &single_qc_ppid_P.igainb)
-// PARAM_ADD(PARAM_FLOAT, dgainb, &single_qc_ppid_P.dgainb)
+PARAM_ADD(PARAM_FLOAT, t_mod, &omni_attitude_controller_P.Constant_Value)
 
-// PARAM_ADD(PARAM_FLOAT, pgainas, &single_qc_ppid_P.pgainas)
-// PARAM_ADD(PARAM_FLOAT, igainas, &single_qc_ppid_P.igainas)
-// PARAM_ADD(PARAM_FLOAT, dgainas, &single_qc_ppid_P.dgainas)
-
-// PARAM_ADD(PARAM_FLOAT, pgainbs, &single_qc_ppid_P.pgainbs)
-// PARAM_ADD(PARAM_FLOAT, igainbs, &single_qc_ppid_P.igainbs)
-// PARAM_ADD(PARAM_FLOAT, dgainbs, &single_qc_ppid_P.dgainbs)
-
-// PARAM_ADD(PARAM_FLOAT, t_mod, &single_qc_ppid_P.torque_modifier)
-
-// PARAM_ADD(PARAM_FLOAT, s_tx, &single_qc_ppid_P.sat_tx)
-// PARAM_ADD(PARAM_FLOAT, s_ty, &single_qc_ppid_P.sat_ty)
-// PARAM_ADD(PARAM_FLOAT, s_tz, &single_qc_ppid_P.sat_tz)
-// PARAM_GROUP_STOP(sparam_ppid)
+PARAM_GROUP_STOP(sparam_omni)
 
