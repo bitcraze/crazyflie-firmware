c2b949702e2a64017f769ad52f39c952359b5cf7
Rik Bouwmeester <rikjbouwmeester@gmail.com>
2024-05-29 15:49:19 +0200

    Fix multi-deck multi-memory OTA flashing

--- test/deck/core/test_deck_memory.c
+++ test/deck/core/test_deck_memory.c
@@ -448,7 +448,7 @@ void testBaseAddressSecondDeckPrimary() {
     deckInfo_ExpectAndReturn(0, &stockInfo);
     deckInfo_ExpectAndReturn(1, &stockInfo);
 
-    uint32_t expected = DECK_MEM_MAP_SIZE * 2;
+    uint32_t expected = DECK_MEM_MAP_SIZE * 3;
 
     // Test
     handleMemRead(0, BUF_SIZE, buffer);
@@ -468,7 +468,7 @@ void testBaseAddressSecondDeckSecondary() {
 
     stockDriver.memoryDefSecondary = &stockSecondaryMemDef;
 
-    uint32_t expected = DECK_MEM_MAP_SIZE * 2 + DECK_MEM_MAP_SIZE;
+    uint32_t expected = DECK_MEM_MAP_SIZE * 3 + DECK_MEM_MAP_SIZE;
 
     // Test
     handleMemRead(0, BUF_SIZE, buffer);
@@ -624,6 +624,24 @@ void testWriteToSecondaryDeckMemory() {
     TEST_ASSERT_TRUE(actual);
 }
 
+void testWriteToSecondDeckPrimaryMemory() {
+    // // Fixture
+    stockPrimaryMemDef.write = mockWrite;
+
+    deckCount_ExpectAndReturn(2);
+    deckInfo_ExpectAndReturn(1, &stockInfo);
+
+    // Test
+    bool actual = handleMemWrite(DECK_MEM_MAP_SIZE * 3 + 100, 30, buffer);
+
+    // Assert
+    TEST_ASSERT_TRUE(write_isCalled);
+    TEST_ASSERT_EQUAL_UINT32(100, write_vAddr);
+    TEST_ASSERT_EQUAL_UINT8(30, write_len);
+    TEST_ASSERT_TRUE(actual);
+}
+
+
 void testWriteToSecondaryDeckMemoryPassesInMemDef() {
     // Fixture
     stockSecondaryMemDef.write = mockWrite;
@@ -637,6 +655,24 @@ void testWriteToSecondaryDeckMemoryPassesInMemDef() {
 
     // Assert
     TEST_ASSERT_EQUAL_PTR(stockDriver.memoryDefSecondary, write_memDef);
+}
+
+void testWriteToSecondDeckSecondaryMemory() {
+    // Fixture
+    stockSecondaryMemDef.write = mockWrite;
+    stockDriver.memoryDefSecondary = &stockSecondaryMemDef;
+
+    deckCount_ExpectAndReturn(2);
+    deckInfo_ExpectAndReturn(1, &stockInfo);
+
+    // Test
+    bool actual = handleMemWrite(DECK_MEM_MAP_SIZE * 4 + 100, 30, buffer);
+
+    // Assert
+    TEST_ASSERT_TRUE(write_isCalled);
+    TEST_ASSERT_EQUAL_UINT32(100, write_vAddr);
+    TEST_ASSERT_EQUAL_UINT8(30, write_len);
+    TEST_ASSERT_TRUE(actual);
 }
 
 void testWriteToDeckWithoutWriteFunction() {
