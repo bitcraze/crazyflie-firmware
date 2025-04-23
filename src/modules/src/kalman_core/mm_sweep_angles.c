038bad7edfe9d4e8392de305f93edf607fc68468
Rik Bouwmeester <rikjbouwmeester@gmail.com>
2025-04-23 11:37:21 +0200

    Lighthouse measurement model clarifications

--- src/modules/src/kalman_core/mm_sweep_angles.c
+++ src/modules/src/kalman_core/mm_sweep_angles.c
@@ -30,22 +30,21 @@ void kalmanCoreUpdateWithSweepAngles(kalmanCoreData_t *this, sweepAngleMeasureme
   // In order to find the sensor's position in the global reference frame, we first calculate the
   // sensor's temporary position in the global reference frame due to Crazyflie's rotation.
   // Then, we add the result to the Crazyflie's global position.
-
-  // Temporary sensor position in global reference frame due to the rotation of the Cf.
-  vec3d temp_ps;
-  arm_matrix_instance_f32 temp_ps_ = {3, 1, temp_ps};
+  // Relative sensor position in global reference frame
+  vec3d ps;
+  arm_matrix_instance_f32 ps_ = {3, 1, ps};
   // Rotation matrix from Crazyflie to global reference frame
   arm_matrix_instance_f32 Rcf_ = {3, 3, (float32_t *)this->R};
-  // Sensor position in Crazyflie reference frame
+  // Relative sensor position in Crazyflie reference frame
   arm_matrix_instance_f32 scf_ = {3, 1, (float32_t *)*sweepInfo->sensorPos};
-  // Rotate the temporary sensor position to the global reference frame
-  mat_mult(&Rcf_, &scf_, &temp_ps_);
 
-  // Crazyflie position in global reference frame
-  vec3d pcf = {this->S[KC_STATE_X], this->S[KC_STATE_Y], this->S[KC_STATE_Z]};
+  // Rotate the relative sensor position to the global reference frame
+  mat_mult(&Rcf_, &scf_, &ps_);
 
   // Sensor position in global reference frame
-  vec3d ps = {pcf[0] + temp_ps[0], pcf[1] + temp_ps[1], pcf[2] + temp_ps[2]};
+  // Gets the current state values of the position of the Crazyflie in the global reference frame and add the relative sensor pos
+  vec3d ps = {this->S[KC_STATE_X] + ps[0], this->S[KC_STATE_Y] + ps[1], this->S[KC_STATE_Z] + ps[2]};
+
   // Rotor position in global reference frame
   const vec3d* pr = sweepInfo->rotorPos;
 
@@ -75,12 +74,12 @@ void kalmanCoreUpdateWithSweepAngles(kalmanCoreData_t *this, sweepAngleMeasureme
   const float error = measuredSweepAngle - predictedSweepAngle;
 
   if (outlierFilterLighthouseValidateSweep(sweepOutlierFilterState, r, error, nowMs)) {
-    // Calculate H vector that contains the Position and Orientation Jacobians in the global reference frame
+    // Calculate H vector (in the rotor reference frame)
     const float z_tan_t = z * tan_t;
     const float qDen = r2 - z_tan_t * z_tan_t;
     // Avoid singularity
     if (qDen > 0.0001f) {
-      // Position Jacobians: ∂α/∂X, ∂α/∂Y, ∂α/∂Z, where X, Y, Z are the sensor position in the global reference frame
+      // Position Jacobians: ∂α/∂x, ∂α/∂y, ∂α/∂z, where x y and z are the sensor position in the global reference frame
 
       const float q = tan_t / arm_sqrt(qDen);
 
@@ -112,12 +111,12 @@ void kalmanCoreUpdateWithSweepAngles(kalmanCoreData_t *this, sweepAngleMeasureme
       // Then, our orientation Jacobians are: ∂α/∂(δφ), ∂α/∂(δθ), ∂α/∂(δψ)
       //
       // Applying the chain rule (e.g. for ∂α/∂(∂φ)), we get:
-      // ∂α/∂(∂φ) = ∂α/∂X * ∂X/∂(δφ) + ∂α/∂Y * ∂Y/∂(δφ) + ∂α/∂Z * ∂Z/∂(δφ)
+      // ∂α/∂(∂φ) = ∂α/∂x * ∂x/∂(δφ) + ∂α/∂y * ∂y/∂(δφ) + ∂α/∂z * ∂z/∂(δφ)
       //
       // We know ∂α/∂X, ∂α/∂Y, ∂α/∂Z from the position Jacobians
       // We still need the partial derivatives of position w.r.t the orientation error
       //
-      // We need ∂X/∂(δφ), ∂Y/∂(δφ), ∂Z/∂(δφ), ∂X/∂(δθ), ∂Y/∂(δθ), ∂Z/∂(δθ), ∂X/∂(δψ), ∂Y/∂(δψ), ∂Z/∂(δψ)
+      // We need ∂x/∂(δφ), ∂z/∂(δφ), ∂x/∂(δθ), ∂z/∂(δθ), ∂x/∂(δψ), ∂z/∂(δψ)
       // These can be derived from the rotation matrices.
       // Let's fall back to the Crazyflie body frame for the orientation error, because we know how to derive the rotation matrices in that frame
       //
@@ -133,17 +132,17 @@ void kalmanCoreUpdateWithSweepAngles(kalmanCoreData_t *this, sweepAngleMeasureme
       // R(ψ) = [sin(ψ)   cos(ψ)  0]
       //        [  0        0     1]
 
-      float dx_droll = 0.0f; // ∂X/∂(δφ)
-      float dy_droll = -ps[2]; // ∂Y/∂(δφ)
-      float dz_droll = ps[1]; // ∂Z/∂(δφ)
+      float dx_droll = 0.0f; // ∂x/∂(δφ)
+      float dy_droll = -ps[2]; // ∂y/∂(δφ)
+      float dz_droll = ps[1]; // ∂z/∂(δφ)
 
-      float dx_dpitch = -ps[2]; // ∂X/∂(δθ)
-      float dy_dpitch = 0.0f; // ∂Y/∂(δθ)
-      float dz_dpitch = ps[0]; // ∂Z/∂(δθ)
+      float dx_dpitch = -ps[2]; // ∂x/∂(δθ)
+      float dy_dpitch = 0.0f; // ∂y/∂(δθ)
+      float dz_dpitch = ps[0]; // ∂z/∂(δθ)
 
-      float dx_dyaw = -ps[1]; // ∂X/∂(δψ)
-      float dy_dyaw = ps[0]; // ∂Y/∂(δψ)
-      float dz_dyaw = 0.0f; // ∂Z/∂(δψ)
+      float dx_dyaw = -ps[1]; // ∂x/∂(δψ)
+      float dy_dyaw = ps[0]; // ∂y/∂(δψ)
+      float dz_dyaw = 0.0f; // ∂z/∂(δψ)
 
       // Update the H vector with Orientation Jacobians (in the global reference frame)
       h[KC_STATE_D0] = g[0]*dx_droll + g[1]*dy_droll + g[2]*dz_droll;
