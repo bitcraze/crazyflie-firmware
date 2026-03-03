01804674572d1c1b58afb1df6aeeccf63b7d9712
ahmedmusatudresden <ahmed.musa@tu-dresden.com>
2026-03-03 17:35:48 +0100

    add tof flow and distance python bindings

--- bindings/setup.py
+++ bindings/setup.py
@@ -52,6 +52,9 @@ fw_sources = [
     "src/modules/src/kalman_core/mm_tdoa.c",
     "src/modules/src/kalman_core/mm_pose.c",
     "src/modules/src/outlierfilter/outlierFilterTdoa.c",
+    "src/modules/src/kalman_core/mm_tof.c",        # <-- Add this
+    "src/modules/src/kalman_core/mm_flow.c",       # <-- Add this
+    "src/modules/src/kalman_core/mm_distance.c",   # <-- Add this
 ]
 
 cffirmware = Extension(
