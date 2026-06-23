026ab54533096e29dcd87b42cf0131767cccdc5f
Valerio Solito <valeriosolito@libero.it>
2026-06-23 12:11:02 +0200

    aggiustati parametri in self_healing_math.h

--- examples/app_self_healing/src/self_healing_math/self_healing_math.h
+++ examples/app_self_healing/src/self_healing_math/self_healing_math.h
@@ -5,13 +5,13 @@
 
 #define DRONE_MASS 0.029f        // kg
 #define DELTA_T 0.05f          // secondi
-#define D_SAFE 2.0f              // metri (Distanza di sicurezza/target)
-#define V_MAX 1.0f               // m/s
-#define K_ATT 0.5f               // Gain attrazione (da tarare)
-#define K_REP 0.8f               // Gain repulsione (da tarare)
+#define D_SAFE 0.4f              // metri (Distanza di sicurezza/target)
+#define V_MAX 0.5f               // m/s
+#define K_ATT 0.28f               // Gain attrazione (da tarare)
+#define K_REP 0.5f               // Gain repulsione (da tarare)
 #define RSSI_REF -55.0f // RSSI a 1 metro di distanza circa, varia da 45 fino a 60/65.
 #define N_EXPONENT 3.0f // Esponente ideale per campo libero, altrimenti all'interno meglio da 3 a 4.
-#define D_TARGET   0.5f   // distanza di equilibrio della molla (m), da tarare
+#define D_TARGET   0.8f   // distanza di equilibrio della molla (m), da tarare
 
 void compute_force_vector(Neighbor_t* neighbors, uint8_t count, uint8_t my_hop, float* fx, float* fy);
 
